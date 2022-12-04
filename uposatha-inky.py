fce93fbf64c18ad766233f57b58e6db9af8ffeb0
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-04 10:08:03 +0800

    First attempt at displaying the date of the next uposatha.

--- uposatha-inky.py
+++ uposatha-inky.py
@@ -3,6 +3,17 @@
 from inky import InkyWHAT
 from PIL import Image, ImageFont, ImageDraw
 from font_fredoka_one import FredokaOne
+import icalendar
+from forest_sangha_moons.MahanikayaCalendar import MahanikayaCalendar
+
+with open("mahanikaya.ical", "r") as f:
+    content = f.read()
+
+ical = icalendar.Calendar.from_ical(content)
+calendar = MahanikayaCalendar()
+calendar.import_ical(ical)
+next_uposatha = calendar.next_uposatha()
+uposatha_date = next_uposatha.date.strftime("%A, %d %b %y")
 
 what = InkyWHAT('yellow')
 what.set_border(what.WHITE)
@@ -11,11 +22,10 @@ img = Image.new("P", (what.WIDTH, what.HEIGHT))
 draw = ImageDraw.Draw(img)
 font = ImageFont.truetype(FredokaOne, 36)
 
-message = "Hello, World!"
-w, h = font.getsize(message)
+w, h = font.getsize(uposatha_date)
 x = (what.WIDTH / 2) - (w / 2)
 y = (what.HEIGHT / 2) - (h / 2)
 
-draw.text((x, y), message, what.RED, font)
+draw.text((x, y), message, what.BLACK, font)
 what.set_image(img)
 what.show()
