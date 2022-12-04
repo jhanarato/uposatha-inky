1da5eff0fba7409e65b45e874554a2ef5146285f
Ajahn Jhanarato <jhanarato@gmail.com>
2022-12-04 12:36:24 +0800

    Created new functions to display in different ways.

--- pillow_play.py
+++ pillow_play.py
@@ -5,19 +5,29 @@ from font_fredoka_one import FredokaOne
 
 WIDTH = 400
 HEIGHT = 300
-BLACK = 1
 
 img = Image.new(mode="RGB",
                 size=(WIDTH, HEIGHT),
                 color=(255, 255, 255))
 
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(FredokaOne, 36)
 
-message = "Hello, World!"
-w, h = font.getsize(message)
-x = (WIDTH / 2) - (w / 2)
-y = (HEIGHT / 2) - (h / 2)
+def draw_centred_text(message):
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(FredokaOne, 36)
+    w, h = font.getsize(message)
+    x = (WIDTH / 2) - (w / 2)
+    y = (HEIGHT / 2) - (h / 2)
+    draw.text((x, y), message, (0, 0, 0), font)
 
-draw.text((x, y), message, (0, 0, 0), font)
+
+def draw_multiline_text(message):
+    draw = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype(FredokaOne, 36)
+    draw.multiline_text(xy=(10, 10),
+                        text=message,
+                        font=fnt,
+                        fill=(0, 0, 0))
+
+
+draw_multiline_text("Hello\nWorld")
 img.show()
