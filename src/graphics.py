c3e7d89a391f9d4b7f2a2984bc044f52696ccb29
Ajahn Jhanarato <jhanarato@gmail.com>
2023-02-09 11:48:38 +0800

    Tidying.

--- src/graphics.py
+++ src/graphics.py
@@ -1,32 +1,17 @@
-from inky.auto import auto
-
 from PIL import Image, ImageDraw, ImageFont
 from font_fredoka_one import FredokaOne
 
-def make_image() -> Image:
+def make_image(text: str) -> Image:
     image = Image.new(mode="P", size=(400, 300))
+
     draw = ImageDraw.Draw(image)
+
     font = ImageFont.truetype(FredokaOne, 36)
-    draw.text((0, 0), "*** TESTING***", 1, font)
+
+    w, h = font.getsize(text)
+    x = (400 / 2) - (w / 2)
+    y = (300 / 2) - (h / 2)
+
+    draw.text((x, y), text, 1, font)
+
     return image
-
-def display_on_screen(image: Image) -> None:
-    palette = [
-        255, 255, 255, # 1 = WHITE
-        0, 0, 0,       # 2 = BLACK
-        255, 255, 0    # 3 = YELLOW
-    ]
-    image.putpalette(palette)
-    converted = image.convert(mode="RGB")
-    converted.show()
-
-def display_on_inky(image: Image) -> None:
-    display = auto()
-    display.set_border(display.WHITE)
-    display.set_image(image)
-    display.show()
-
-if __name__ == "__main__":
-    image = make_image()
-    display_on_inky(image)
-    # display_on_screen(image)
---
+++ src/uposatha_inky.py
@@ -0,0 +1,37 @@
+# Script to display the uposatha on a Pimoroni Inky WHAT e-ink screen.
+from PIL import Image
+from inky import auto
+
+from uposatha.calendar import Calendar
+from data import next_uposatha_view
+from graphics import make_image
+
+def display_on_screen(image: Image) -> None:
+    palette = [
+        255, 255, 255, # 1 = WHITE
+        0, 0, 0,       # 2 = BLACK
+        255, 255, 0    # 3 = YELLOW
+    ]
+    image.putpalette(palette)
+    converted = image.convert(mode="RGB")
+    converted.show()
+
+
+def display_on_inky(image: Image) -> None:
+    display = auto()
+    display.set_border(display.WHITE)
+    display.set_image(image)
+    display.show()
+
+def main():
+    calendar = Calendar()
+    view = next_uposatha_view(calendar)
+    uposatha_date = view.falls_on.strftime("%d/%m/%y")
+
+    image = make_image(uposatha_date)
+    # display_on_inky(image)
+    display_on_screen(image)
+
+
+if __name__ == "__main__":
+    main()
