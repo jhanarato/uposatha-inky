873c3730bb89f57672ce937381c1877cda396c7d
Ajahn Jhanarato <jhanarato@gmail.com>
2023-02-12 08:24:19 +0800

    Added script to turn off LEDs on the raspberry pi.

---
+++ config/leds_off.sh
@@ -0,0 +1,4 @@
+sudo sh -c 'echo none > /sys/class/leds/led0/trigger'
+sudo sh -c 'echo none > /sys/class/leds/led1/trigger'
+sudo sh -c 'echo 0 > /sys/class/leds/led0/brightness'
+sudo sh -c 'echo 0 > /sys/class/leds/led1/brightness'
