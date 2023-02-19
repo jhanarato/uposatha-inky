4131f9cc742e7ca87740353c5b21f014a5913849
Ajahn Jhanarato <jhanarato@gmail.com>
2023-02-19 09:37:20 +0800

    Messing around with heading font.

--- src/images.py
+++ src/images.py
@@ -1,5 +1,12 @@
+from typing import List
+
 from PIL import Image, ImageDraw, ImageFont
 from font_fredoka_one import FredokaOne
+from font_source_serif_pro import SourceSerifProBoldIt
+from font_caladea import CaladeaBoldItalic
+from font_font_awesome import FontAwesome5Free
+from font_hanken_grotesk import HankenGroteskBold
+from font_manrope import ManropeBold
 
 from content import NextUposathaContent
 
@@ -12,11 +19,20 @@ YELLOW = 2
 
 def make_image(content: NextUposathaContent) -> Image:
     image = Image.new(mode="P", size=(WIDTH, HEIGHT), color=WHITE)
-    draw_centered_text(image, 0, "Next Uposatha:")
-    draw_centered_text(image, 50, content.day)
-    draw_centered_text(image, 100, content.date)
-    draw_centered_text(image, 150, content.days_until)
+    draw_heading(image, "Next Uposatha")
+    # draw_centered_text(image, 50, content.day)
+    # draw_centered_text(image, 100, content.date)
+    # draw_centered_text(image, 150, content.days_until)
     return image
+
+def draw_heading(image: Image, text: str) -> None:
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype(ManropeBold, 36)
+    x_coord = centered_x_coord(font, text)
+    draw.text((x_coord, 0), text, BLACK, font)
+
+def draw_content(image: Image, text_lines: List[str]) -> None:
+    pass
 
 def draw_centered_text(image: Image, y_coord: int, text: str):
     draw = ImageDraw.Draw(image)
