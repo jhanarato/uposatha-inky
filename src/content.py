6d7b85000d94194f3b493513edba4de118ba5002
Ajahn Jhanarato <jhanarato@gmail.com>
2023-02-19 10:04:13 +0800

    Heading looks good now. Reworking content as multiline text.

--- src/content.py
+++ src/content.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from dataclasses import dataclass
 from datetime import date
 from uposatha.calendar import Calendar
@@ -8,14 +10,16 @@ class NextUposathaContent:
     date: str
     days_until: str
 
-def next_uposatha_content(today: date) -> NextUposathaContent:
+def next_uposatha_content(today: date) -> List[str]:
     calendar = Calendar()
     next_uposatha = calendar.next_uposatha(today)
 
     days_until = (next_uposatha.falls_on - today).days
 
-    return NextUposathaContent(
-        day = next_uposatha.falls_on.strftime("%A"),
-        date = next_uposatha.falls_on.strftime("%d/%m/%y"),
-        days_until = f"In {days_until} days"
-    )
+    # return NextUposathaContent(
+    #     day = next_uposatha.falls_on.strftime("%A"),
+    #     date = next_uposatha.falls_on.strftime("%d/%m/%y"),
+    #     days_until = f"In {days_until} days"
+    # )
+
+    return ["Line 1", "Line 2"]
--- src/images.py
+++ src/images.py
@@ -17,25 +17,31 @@ WHITE = 0
 BLACK = 1
 YELLOW = 2
 
-def make_image(content: NextUposathaContent) -> Image:
+def make_image(content: List[str]) -> Image:
     image = Image.new(mode="P", size=(WIDTH, HEIGHT), color=WHITE)
-    draw_heading(image, "Next Uposatha")
+    draw = ImageDraw.Draw(image)
+    draw_heading(draw, "Next Uposatha")
+    draw_underline(draw)
+    draw_content(draw, content)
+
     # draw_centered_text(image, 50, content.day)
     # draw_centered_text(image, 100, content.date)
     # draw_centered_text(image, 150, content.days_until)
     return image
 
-def draw_heading(image: Image, text: str) -> None:
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(CaladeaBoldItalic, 36)
+def draw_heading(draw: ImageDraw, text: str) -> None:
+    font = ImageFont.truetype(CaladeaBoldItalic, 40)
     x_coord = centered_x_coord(font, text)
-    draw.text((x_coord, 0), text, BLACK, font)
+    draw.text((x_coord, 10), text, BLACK, font)
 
-def draw_content(image: Image, text_lines: List[str]) -> None:
+def draw_underline(draw: ImageDraw):
+    y_coord = 70
+    draw.line([50, y_coord, WIDTH - 50, y_coord], BLACK, 2)
+
+def draw_content(draw: ImageDraw, text_lines: List[str]) -> None:
     pass
 
-def draw_centered_text(image: Image, y_coord: int, text: str):
-    draw = ImageDraw.Draw(image)
+def draw_centered_text(draw: ImageDraw, y_coord: int, text: str):
     font = ImageFont.truetype(FredokaOne, 36)
     text_coord = (centered_x_coord(font, text), y_coord)
     draw.text(text_coord, text, BLACK, font)
