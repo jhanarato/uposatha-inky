4ebf9d810826fead76e95bb0da82fe713fe7e784
Ajahn Jhanarato <jhanarato@gmail.com>
2023-02-20 09:04:04 +0800

    Cleaned up next_uposatha_content() using the string.Template class.

--- src/content.py
+++ src/content.py
@@ -1,28 +1,22 @@
-from typing import List
-
-from dataclasses import dataclass
+from string import Template
 from datetime import date
 from uposatha.calendar import Calendar
-from uposatha.elements import Season, Uposatha
 
-def next_uposatha_content(today: date) -> List[str]:
+def next_uposatha_content(today: date) -> str:
     calendar = Calendar()
     uposatha = calendar.next_uposatha(today)
     season = calendar.current_season(today)
 
-    return [
-        line_one(uposatha, today),
-        line_two(season, uposatha)
-    ]
-
-def line_one(uposatha, today):
-    formatted_date = uposatha.falls_on.strftime("%a %d/%m")
-    text = f"{formatted_date} ({uposatha.days_since_previous} Day)"
-    return text
-
-def line_two(season: Season, uposatha: Uposatha):
-    text = (
-        f"{uposatha.number_in_season}/{len(season.uposathas)} "
-        f"{season.name.name.capitalize()} Season"
+    template = Template(
+        "${formatted_date} (${days_since_previous} Day)\n"
+        "${uposatha_number}/${number_of_uposathas} "
+        "${season_name} Season"
     )
-    return text
+
+    return template.substitute(
+        formatted_date=uposatha.falls_on.strftime("%a %d/%m"),
+        days_since_previous=uposatha.days_since_previous,
+        uposatha_number=uposatha.number_in_season,
+        number_of_uposathas=len(season.uposathas),
+        season_name=season.name.name.capitalize()
+    )
--- src/images.py
+++ src/images.py
@@ -1,12 +1,6 @@
 from typing import List
 
 from PIL import Image, ImageDraw, ImageFont
-from font_fredoka_one import FredokaOne
-from font_source_serif_pro import SourceSerifProBold
-from font_caladea import CaladeaBoldItalic, CaladeaBold
-from font_font_awesome import FontAwesome5Free
-from font_hanken_grotesk import HankenGroteskBold
-from font_manrope import ManropeBold
 from font_roboto import RobotoBold
 
 WIDTH = 400
@@ -16,16 +10,16 @@ WHITE = 0
 BLACK = 1
 YELLOW = 2
 
-def make_image(content: List[str]) -> Image:
+def make_image(content: str) -> Image:
     image = Image.new(mode="P", size=(WIDTH, HEIGHT), color=WHITE)
     draw = ImageDraw.Draw(image)
-    draw_heading(draw, "Next Uposatha")
+    draw_heading(draw, "Uposatha")
     draw_underline(draw)
     draw_content(draw, content)
     return image
 
 def draw_heading(draw: ImageDraw, text: str) -> None:
-    font = ImageFont.truetype(CaladeaBoldItalic, 40)
+    font = ImageFont.truetype(font=RobotoBold, size=36)
     x_coord = centered_x_coord(font, text)
     y_coord = 10
     draw.text((x_coord, y_coord), text, BLACK, font)
@@ -34,9 +28,9 @@ def draw_underline(draw: ImageDraw):
     y_coord = 70
     draw.line([50, y_coord, WIDTH - 50, y_coord], BLACK, 2)
 
-def draw_content(draw: ImageDraw, text_lines: List[str]) -> None:
-    font = ImageFont.truetype(RobotoBold, 32)
-    text = "\n".join(text_lines)
+def draw_content(draw: ImageDraw, text: str) -> None:
+    font = ImageFont.truetype(font=RobotoBold,
+                              size=32)
     y_coord = 120
     _, _, width, _ = draw.textbbox((0,0), text, font)
     x_coord = (WIDTH / 2) - (width / 2)
@@ -46,7 +40,8 @@ def draw_content(draw: ImageDraw, text_lines: List[str]) -> None:
         text=text,
         font=font,
         fill=BLACK,
-        align="center"
+        align="center",
+        spacing=20
     )
 
 def centered_x_coord(font: ImageFont, text: str) -> int:
