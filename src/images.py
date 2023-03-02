635377fd467fc30480a1586aaa75197016f1b954
Ajahn Jhanarato <jhanarato@gmail.com>
2023-03-02 10:21:03 +0800

    Lots of cleaning and refactoring in images.py
    Tests pass and everyone is happy.

--- src/images.py
+++ src/images.py
@@ -1,4 +1,3 @@
-from collections.abc import Iterator
 from typing import List, Tuple
 from dataclasses import dataclass
 from PIL import Image, ImageDraw, ImageFont
@@ -6,68 +5,70 @@ from font_roboto import RobotoBold
 
 from content import NextUposatha
 
-WIDTH = 400
-HEIGHT = 300
-
-WHITE = 0
-BLACK = 1
-YELLOW = 2
+@dataclass(frozen=True)
+class DrawingConfig:
+    height: int = 300
+    width:  int = 400
+    white:  int = 0
+    black:  int = 1
+    yellow: int = 2
 
 def make_image(content: NextUposatha) -> Image:
-    image = Image.new(mode="P", size=(WIDTH, HEIGHT), color=WHITE)
+    config = DrawingConfig()
+    image = Image.new(mode="P", size=(config.width, config.height), color=config.white)
     draw = ImageDraw.Draw(image)
-    draw_heading(draw, "Uposatha")
-    draw_underline(draw, y_coord=70)
-    draw_info(draw, content.info)
-    draw_countdown(draw, content.countdown)
+    draw_heading(draw, config ,"Uposatha")
+    draw_underline(draw, config, y_coord=70)
+    draw_info(draw, config, content.info)
+    draw_countdown(draw, config, content.countdown)
     return image
 
-def draw_heading(draw: ImageDraw, text: str) -> None:
+def draw_heading(draw: ImageDraw, config: DrawingConfig, text: str) -> None:
     font = ImageFont.truetype(font=RobotoBold, size=36)
-    x_coord = centered_x_coord(font.getlength(text))
+    x_coord = centered_x_coord(config.width, font.getlength(text))
     y_coord = 10
-    draw.text((x_coord, y_coord), text, BLACK, font)
+    draw.text((x_coord, y_coord), text, config.black, font)
 
-def draw_underline(draw: ImageDraw, y_coord: int):
-    draw.line([50, y_coord, WIDTH - 50, y_coord], BLACK, 2)
+def draw_underline(draw: ImageDraw, config: DrawingConfig, y_coord: int):
+    draw.line([50, y_coord, config.width - 50, y_coord], config.black, 2)
 
-def draw_info(draw: ImageDraw, text: str) -> None:
+def draw_info(draw: ImageDraw, config: DrawingConfig, text: str) -> None:
     font = ImageFont.truetype(font=RobotoBold, size=32)
     y_coord = 90
-    width = draw.textbbox((0,0), text, font)[2]
-    x_coord = centered_x_coord(width)
+    text_width = draw.textbbox((0,0), text, font)[2]
+    x_coord = centered_x_coord(config.width, text_width)
 
     draw.multiline_text(
         xy=(x_coord, y_coord),
         text=text,
         font=font,
-        fill=BLACK,
+        fill=config.black,
         align="center",
         spacing=10
     )
 
-def draw_countdown(draw: Image, letters: List[str]) -> None:
+def draw_countdown(draw: Image, config: DrawingConfig, letters: List[str]) -> None:
     centres = centre_points(
         y_coord=220,
-        screen_width=WIDTH,
+        screen_width=config.width,
         spacing=20,
         number_of_points=len(letters)
     )
 
     for letter, centre in zip(letters, centres):
-        draw_letter(draw, letter, centre)
+        draw_letter(draw, config.black, letter, centre)
 
-def draw_letter(draw: Image, letter: str, centre: Tuple[int, int]):
+def draw_letter(draw: Image, colour: int, letter: str, centre: Tuple[int, int]):
     font = ImageFont.truetype(font=RobotoBold, size=20)
     draw.text(
         xy=centre,
         text=letter,
         font=font,
-        fill=BLACK
+        fill=colour
     )
 
-def centered_x_coord(object_width: int) -> int:
-    return round((WIDTH / 2) - (object_width / 2))
+def centered_x_coord(screen_width: int, object_width: int) -> int:
+    return round((screen_width / 2) - (object_width / 2))
 
 def centre_points(y_coord: int,
                   screen_width: int,
--- tests/test_countdown.py
+++ tests/test_countdown.py
@@ -1,8 +1,7 @@
 import pytest
 from datetime import date, timedelta
-from itertools import islice
-from content import countdown_letters, split_countdown
-from images import letter_coords, CountdownArea, centre_points
+from content import countdown_letters
+from images import centre_points
 
 
 def test_letters_fifteen():
@@ -21,64 +20,6 @@ def test_letters_on_day():
     a_friday = date(2010, 3, 26)
     uposatha = a_friday
     assert countdown_letters(a_friday, uposatha) == ["F"]
-
-@pytest.mark.parametrize(
-    "days_left,countdown",
-    [
-        (14, (["T", "F", "S", "S", "M", "T", "W"],
-              ["T", "F", "S", "S", "M", "T", "W", "T"])),
-
-        (8, (["W"], ["T", "F", "S", "S", "M", "T", "W", "T"])),
-        (7, ([], ["T", "F", "S", "S", "M", "T", "W", "T"])),
-        (6, ([], ["F", "S", "S", "M", "T", "W", "T"])),
-        (1, ([], ["W", "T"])),
-        (0, ([], ["T"])) # The uposatha
-    ]
-)
-def test_split(days_left, countdown):
-    end = date(2011, 2, 3) # 15 day new moon.
-    start = end - timedelta(days_left)
-    letters = countdown_letters(start, end)
-    actual = split_countdown(letters)
-    assert actual.top_row == countdown[0]
-    assert actual.bottom_row == countdown[1]
-
-@pytest.fixture
-def countdown_area():
-    return CountdownArea(
-        x=30,
-        y=50,
-        width=240,
-        height=100,
-        border=10,
-        letter_spacing=20,
-        row_spacing=20
-    )
-
-
-@pytest.mark.parametrize(
-    "letter_num,row_num,x",
-    [
-        (0, 0, 260),
-        (1, 0, 240),
-        (1, 1, 240),
-        (2, 0, 220)
-    ]
-)
-def test_countdown_letter_x(countdown_area, letter_num, row_num, x):
-    assert letter_coords(countdown_area, letter_num, row_num)[0] == x
-
-
-@pytest.mark.parametrize(
-    "letter_num,row_num,y",
-    [
-        (0, 0, 50),
-        (1, 0, 50),
-        (0, 1, 70)
-    ]
-)
-def test_countdown_letter_y(countdown_area, letter_num, row_num, y):
-    assert letter_coords(countdown_area, letter_num, row_num)[1] == y
 
 @pytest.mark.parametrize(
     "y_coord,width,spacing,number,result",
