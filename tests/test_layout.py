cf07488c34c68d1b23133ffbc50260aa9b76b351
Ajahn Jhanarato <jhanarato@gmail.com>
2023-03-05 10:40:54 +0800

    Commented.

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -8,4 +8,4 @@ def test_add_centred_text():
     layout = Layout(DrawingConfig())
     text = Text("Happy Birthday")
     layout.add_text(text)
-    assert layout.items[0].x == 76
+    assert layout.items[0].x == 76 # Given RobotoBold 32pt
