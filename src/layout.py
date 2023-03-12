e4d130687e511dc73bce05ea83b002c7d472df37
Ajahn Jhanarato <jhanarato@gmail.com>
2023-03-12 12:50:55 +0800

    Inlined x computation methods.

--- src/layout.py
+++ src/layout.py
@@ -20,19 +20,13 @@ class Layout:
     def add(self, component: ImageComponent, align: Align) -> None:
         self.components.append((component, align))
 
-    def center_aligned_x(self, component: ImageComponent) -> int:
-        return int((self.screen_width - component.width()) / 2)
-
-    def right_aligned_x(self, component: ImageComponent) -> int:
-        return int((self.screen_width - component.width()))
-
     def align_x(self, component: ImageComponent, align: Align) -> int:
         if align == Align.LEFT:
             return 0
         if align == Align.CENTRE:
-            return self.center_aligned_x(component)
+            return int((self.screen_width - component.width()) / 2)
         if align == Align.RIGHT:
-            return self.right_aligned_x(component)
+            return int((self.screen_width - component.width()))
 
     def draw(self) -> None:
         for component, align in self.components:
