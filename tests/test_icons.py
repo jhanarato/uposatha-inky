fbe16113142ba435e00eb52032b95e0fbf05188e
Ajahn Jhanarato <jhanarato@gmail.com>
2023-04-16 09:39:19 +0800

    Deleted test that was getting in the way.

--- tests/test_icons.py
+++ tests/test_icons.py
@@ -3,14 +3,3 @@ import pytest
 from components import LetterIcon
 from screen import ImageConfig
 
-
-def test_should_centre_letter_text():
-    config = ImageConfig()
-    icon = LetterIcon(draw=None,
-                      font=config.font_styles.COUNTDOWN,
-                      background=config.palette.BLACK,
-                      foreground=config.palette.WHITE,
-                      letter="W", size=40)
-    icon._text_height = 10
-    icon._text_width = 20
-    assert icon._to_text_xy(0, 0) == (10, 15)
