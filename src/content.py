8b24b531d614cb2acbc0e8da155b1bbd61194001
Ajahn Jhanarato <jhanarato@gmail.com>
2023-04-23 09:39:40 +0800

    Content includes moon phase. Changed shadowed variable uposatha.

--- src/content.py
+++ src/content.py
@@ -1,28 +1,34 @@
 from typing import List
 from dataclasses import dataclass
 from datetime import date, timedelta
+
+import uposatha.elements
 from uposatha.calendar import Calendar
+from uposatha.elements import MoonPhase
+
 
 @dataclass
 class NextUposatha:
     date: str
     details: str
     countdown: List[str]
+    moon_phase: uposatha.elements.MoonPhase
 
 def next_uposatha_content(today: date) -> NextUposatha:
     calendar = Calendar()
-    uposatha = calendar.next_uposatha(today)
+    next_uposatha = calendar.next_uposatha(today)
     season = calendar.current_season(today)
 
     return NextUposatha(
-        date=uposatha.falls_on.strftime("%a %d/%m/%y"),
-        details=uposatha_details(season, uposatha),
-        countdown=countdown_letters(today, uposatha.falls_on)
+        date=next_uposatha.falls_on.strftime("%a %d/%m/%y"),
+        details=uposatha_details(season, next_uposatha),
+        countdown=countdown_letters(today, next_uposatha.falls_on),
+        moon_phase=MoonPhase.FULL
     )
 
-def uposatha_details(season, uposatha):
-    days_since_previous = uposatha.days_since_previous
-    uposatha_number = uposatha.number_in_season
+def uposatha_details(season, next_uposatha):
+    days_since_previous = next_uposatha.days_since_previous
+    uposatha_number = next_uposatha.number_in_season
     number_of_uposathas = len(season.uposathas)
     season_name = season.name.name.capitalize()
     return f"{uposatha_number} of {number_of_uposathas} | {season_name} | {days_since_previous} Day"
