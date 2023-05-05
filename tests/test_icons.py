a92abd81645c4a1b1370f0c1e9f2b8adfe58a07e
Ajahn Jhanarato <jhanarato@gmail.com>
2023-05-05 18:05:11 +0800

    Reversing Icons works.

--- tests/test_icons.py
+++ tests/test_icons.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 import pytest
 
 from icons import Icons
@@ -9,13 +10,23 @@ def test_icon_sequence_has_length():
     icons = Icons(None, config, 1, ["M", "T", "W"])
     assert len(icons) == 3
 
-def test_icon_collection_can_be_iterated_over():
+def test_icon_sequence_can_be_iterated_over():
     config = ImageConfig()
     icons = Icons(None, config, 1, ["M", "T", "W"])
     for icon in icons:
         pass
 
-def test_icon_collection_can_be_accessed_by_index():
+def test_icon_sequence_can_be_accessed_by_index():
     config = ImageConfig()
     icons = Icons(None, config, 1, ["M", "T", "W"])
-    assert icons[0].height() == 1
+    assert icons[0].letter == "M"
+
+def test_icon_instance_is_sequence():
+    config = ImageConfig()
+    icons = Icons(None, config, 1, ["M", "T", "W"])
+    assert isinstance(icons, Sequence)
+
+def test_icon_sequence_can_be_reversed():
+    config = ImageConfig()
+    icons = Icons(None, config, 1, ["M", "T", "W"])
+    assert [icon.letter for icon in reversed(icons)] == ["W", "T", "M"]
