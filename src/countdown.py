2668e67f11eaf2b7a9aed3202ba70e1083c5d787
Ajahn Jhanarato <jhanarato@gmail.com>
2023-05-21 10:40:38 +0800

    Removed old style countdown layout.

--- src/countdown.py
+++ src/countdown.py
@@ -14,7 +14,6 @@ class Countdown:
     def __init__(self, icons: CountdownIcons, max_columns: int, gap: int):
         self._icons = icons
         self._gap = gap
-        self._rows = seq_to_rows(icons, max_columns)
         self._grid = IconGrid(icons, max_columns)
 
     def height(self) -> int:
@@ -73,61 +72,3 @@ class IconGrid:
 
 def skip_n(i: Iterator, n: int):
     [next(i) for _ in range(n)]
-
-# TODO Remove code below when grid works.
-
-XY = tuple[int, int]
-
-class CountdownLayout:
-    """ A sub-layout for countdown icons. Likely to be removed soon. """
-
-    def __init__(self, bbox: BoundingBox, icons: CountdownIcons, gap: int):
-        self._bbox = bbox
-        self._icons = icons
-        self._icon_distance = icons.icon_size + gap
-        self._offset = icons.icon_size // 2
-
-        self._x_start = self._bbox.left + self._offset
-        self._y_start = self._bbox.top + self._offset
-
-    def _centers(self) -> list[XY]:
-        return distribute_centers(
-            x_start=self._x_start,
-            y_start=self._y_start,
-            distance=self._icon_distance,
-            number_of_icons=len(self._icons)
-        )
-
-    def _to_xy(self, centers: list[XY]) -> list[XY]:
-        return [(cx - self._offset, cy - self._offset)
-                for cx, cy in centers]
-
-    def draw(self):
-        icons_centers = self._centers()
-        icons_xy = self._to_xy(icons_centers)
-        for icon, xy in zip(self._icons, icons_xy):
-            icon.draw(*xy)
-
-
-def distribute_centers(x_start: int, y_start: int, distance: int, number_of_icons: int) -> list[XY]:
-    return [
-        (x_start + distance * number, y_start)
-        for number in range(number_of_icons)
-    ]
-
-T = TypeVar('T')
-
-def seq_to_rows(seq: Sequence[T], row_length: int) -> list[list[T]]:
-    full_rows, items_left_over = divmod(len(seq), row_length)
-
-    rows = []
-
-    if items_left_over > 0:
-        rows.append(list(seq[:items_left_over]))
-
-    for row_num in range(full_rows):
-        start = items_left_over + row_num * row_length
-        stop = start + row_length
-        rows.append(list(seq[start:stop]))
-
-    return rows
--- tests/test_countdown.py
+++ tests/test_countdown.py
@@ -1,12 +1,10 @@
 from datetime import date, timedelta
-from typing import cast
 
 import pytest
 
-from countdown import Countdown, CountdownLayout, distribute_centers, seq_to_rows, IconGrid, skip_n
-from icons import CountdownIcons, LetterIcon
+from countdown import Countdown, IconGrid, skip_n
+from icons import CountdownIcons
 from content import countdown_letters
-from layout import BoundingBox
 from screen import ImageConfig
 
 
@@ -65,98 +63,16 @@ def three_icon_sequence():
     icons._icons = icon_spies
     return icons
 
-@pytest.mark.parametrize(
-    "number,points",
-    [
-        (1, [(5, 5)]),
-        (2, [(5, 5), (15, 5)]),
-        (3, [(5, 5), (15, 5), (25, 5)]),
-    ]
-)
-def test_should_calculate_center_points(number, points):
-    config = ImageConfig()
-    icon_size = 10
-    icon_spies = [LetterSpy(size=icon_size) for _ in range(number)]
-    icons = CountdownIcons(None, config, icon_size, [])
-    icons._icons = icon_spies
-
-    box = BoundingBox(top=0, left=0, height=30, width=70)
-    layout = CountdownLayout(bbox=box, icons=icons, gap=0)
-    assert layout._centers() == points
-
-def test_should_center_relative_to_bbox(one_icon_sequence):
-    box = BoundingBox(top=10, left=20, height=30, width=70)
-    layout = CountdownLayout(bbox=box, icons=one_icon_sequence, gap=0)
-    assert layout._centers() == [(25, 15)]
-
-def test_should_convert_centers_to_xy(one_icon_sequence):
-    box = BoundingBox(top=0, left=0, height=30, width=70)
-    layout = CountdownLayout(bbox=box, icons=one_icon_sequence, gap=0)
-    assert layout._to_xy([(5, 5), (15, 5), (25, 5)]) == [(0, 0), (10, 0), (20, 0)]
-
-def test_should_draw_icons_at_top_left(three_icon_sequence):
-    box = BoundingBox(top=0, left=0, height=100, width=100)
-
-    layout = CountdownLayout(bbox=box, icons=three_icon_sequence, gap=0)
-    layout.draw()
-
-    drawn_at = [icon.last_draw_at for icon in three_icon_sequence]
-    assert drawn_at == [(0, 0), (10, 0), (20, 0)]
-
 def test_should_create_icon_list():
     config = ImageConfig()
     letters = ["M", "T", "W"]
     icons = CountdownIcons(draw=None, config=config, icon_size=10, letters=letters)
     assert len(icons) == 3
 
-def test_should_draw_icons_with_gap(three_icon_sequence):
-    bbox = BoundingBox(0, 0, 100, 100)
-    layout = CountdownLayout(bbox=bbox, icons=three_icon_sequence, gap=2)
-    layout.draw()
-
-    assert cast(LetterSpy, three_icon_sequence[0]).last_draw_at == (0, 0)
-    assert cast(LetterSpy, three_icon_sequence[1]).last_draw_at == (12, 0)
-    assert cast(LetterSpy, three_icon_sequence[2]).last_draw_at == (24, 0)
-
-@pytest.mark.parametrize(
-    "number,points",
-    [
-        (1, [(5, 5)]),
-        (2, [(5, 5), (15, 5)]),
-        (3, [(5, 5), (15, 5), (25, 5)]),
-    ]
-)
-def test_should_distribute_center_points(number, points):
-    centers = distribute_centers(
-        x_start=5, y_start=5, distance=10, number_of_icons=number
-    )
-    assert centers == points
-
-def test_should_split_sequence_into_two_rows():
-    result = seq_to_rows(seq=[1, 2, 3, 4], row_length=2)
-    assert result == [[1, 2], [3, 4]]
-
-def test_should_split_sequence_with_smaller_first_row():
-    result = seq_to_rows(seq=[1, 2, 3], row_length=2)
-    assert result == [[1], [2, 3]]
-
-def test_should_handle_sequence_equal_to_row_length():
-    result = seq_to_rows(seq=[1, 2, 3], row_length=3)
-    assert result == [[1, 2, 3]]
-
-def test_should_handle_sequence_smaller_than_row_length():
-    result = seq_to_rows(seq=[1, 2, 3], row_length=4)
-    assert result == [[1, 2, 3]]
-
 @pytest.fixture
 def four_icons():
     config = ImageConfig()
     return CountdownIcons(None, config, 10, ["S", "M", "T", "W"])
-
-def test_should_split_rows_if_sequence_is_icons(four_icons):
-    rows = seq_to_rows(seq=four_icons, row_length=3)
-    assert len(rows[0]) == 1
-    assert len(rows[1]) == 3
 
 def test_should_report_height_for_two_rows(four_icons):
     countdown = Countdown(icons=four_icons, gap=2, max_columns=2)
