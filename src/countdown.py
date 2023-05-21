38b63cf39358ad8fa2a4107f3d86016b3c78c6bf
Ajahn Jhanarato <jhanarato@gmail.com>
2023-05-21 08:47:41 +0800

    Moved code to be deleted to end of file.

--- src/countdown.py
+++ src/countdown.py
@@ -60,44 +60,6 @@ def seq_to_rows(seq: Sequence[T], row_length: int) -> list[list[T]]:
 
     return rows
 
-XY = tuple[int, int]
-
-
-class CountdownLayout:
-    """ A sub-layout for countdown icons. Likely to be removed soon. """
-    def __init__(self, bbox: BoundingBox, icons: CountdownIcons, gap: int):
-        self._bbox = bbox
-        self._icons = icons
-        self._icon_distance = icons.icon_size + gap
-        self._offset = icons.icon_size // 2
-
-        self._x_start = self._bbox.left + self._offset
-        self._y_start = self._bbox.top + self._offset
-
-    def _centers(self) -> list[XY]:
-        return distribute_centers(
-            x_start=self._x_start,
-            y_start=self._y_start,
-            distance=self._icon_distance,
-            number_of_icons=len(self._icons)
-        )
-
-    def _to_xy(self, centers: list[XY]) -> list[XY]:
-        return [(cx - self._offset, cy - self._offset)
-                for cx, cy in centers]
-
-    def draw(self):
-        icons_centers = self._centers()
-        icons_xy = self._to_xy(icons_centers)
-        for icon, xy in zip(self._icons, icons_xy):
-            icon.draw(*xy)
-
-
-def distribute_centers(x_start: int, y_start: int, distance: int, number_of_icons: int) -> list[XY]:
-    return [
-        (x_start + distance * number, y_start)
-        for number in range(number_of_icons)
-    ]
 
 @dataclass
 class GridPosition:
@@ -134,3 +96,44 @@ class IconGrid:
 
 def skip_n(i: Iterator, n: int):
     [next(i) for _ in range(n)]
+
+# TODO Remove code below when grid works.
+
+XY = tuple[int, int]
+
+class CountdownLayout:
+    """ A sub-layout for countdown icons. Likely to be removed soon. """
+
+    def __init__(self, bbox: BoundingBox, icons: CountdownIcons, gap: int):
+        self._bbox = bbox
+        self._icons = icons
+        self._icon_distance = icons.icon_size + gap
+        self._offset = icons.icon_size // 2
+
+        self._x_start = self._bbox.left + self._offset
+        self._y_start = self._bbox.top + self._offset
+
+    def _centers(self) -> list[XY]:
+        return distribute_centers(
+            x_start=self._x_start,
+            y_start=self._y_start,
+            distance=self._icon_distance,
+            number_of_icons=len(self._icons)
+        )
+
+    def _to_xy(self, centers: list[XY]) -> list[XY]:
+        return [(cx - self._offset, cy - self._offset)
+                for cx, cy in centers]
+
+    def draw(self):
+        icons_centers = self._centers()
+        icons_xy = self._to_xy(icons_centers)
+        for icon, xy in zip(self._icons, icons_xy):
+            icon.draw(*xy)
+
+
+def distribute_centers(x_start: int, y_start: int, distance: int, number_of_icons: int) -> list[XY]:
+    return [
+        (x_start + distance * number, y_start)
+        for number in range(number_of_icons)
+    ]
