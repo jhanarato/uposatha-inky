6b07242de4f2ed4ee714f900830c60986e659f7c
Ajahn Jhanarato <jhanarato@gmail.com>
2023-05-25 10:21:28 +0800

    Tidied up countdown_letters()

--- src/countdown.py
+++ src/countdown.py
@@ -130,13 +130,6 @@ def skip_n(i: Iterator, n: int):
     [next(i) for _ in range(n)]
 
 
-def countdown_letters(today: date, uposatha_date: date) -> list[str]:
+def countdown_letters(today: date, uposatha: date) -> list[str]:
     return [date_.strftime("%a")[0]
-            for date_ in daterange(today, uposatha_date, inclusive=True)]
-    # day_letters = []
-    # next_date = today
-    # while next_date <= uposatha_date:
-    #     day_letter = next_date.strftime("%a")[0]
-    #     day_letters.append(day_letter)
-    #     next_date += timedelta(1)
-    # return day_letters
+            for date_ in daterange(today, uposatha, inclusive=True)]
