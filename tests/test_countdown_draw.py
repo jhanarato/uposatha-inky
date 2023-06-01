30d7892c58174a753b77c4692ec5d9c6a1c433da
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-01 10:57:25 +0800

    Split countdown tests into resize, grid, draw.

---
+++ tests/test_countdown_draw.py
@@ -0,0 +1,61 @@
+from datetime import date
+
+from uposatha.elements import MoonPhase
+
+from conftest import LetterSpy
+from countdown import Countdown
+from screen import ImageConfig
+
+
+def test_should_draw_icons():
+    icon_size = 10
+    countdown = Countdown(draw=None, config=ImageConfig(),
+                          start=date(2023, 5, 7),
+                          end=date(2023, 5, 10),
+                          moon_phase=MoonPhase.FULL,
+                          icon_size=icon_size, gap=0, max_columns=2)
+
+    spies = [LetterSpy(10) for _ in range(4)]
+    countdown._grid._icons._icons = spies
+    countdown.draw(0, 0)
+
+    assert [spy.last_draw_at for spy in spies] == [
+        (0, 0), (10, 0),
+        (0, 10), (10, 10)
+    ]
+
+
+def test_should_put_gap_between_icons():
+    icon_size = 10
+    countdown = Countdown(draw=None, config=ImageConfig(),
+                          start=date(2023, 5, 7),
+                          end=date(2023, 5, 10),
+                          moon_phase=MoonPhase.FULL,
+                          icon_size=icon_size, gap=2, max_columns=2)
+
+    spies = [LetterSpy(10) for _ in range(4)]
+    countdown._grid._icons._icons = spies
+    countdown.draw(0, 0)
+
+    assert [spy.last_draw_at for spy in spies] == [
+        (0, 0), (12, 0),
+        (0, 12), (12, 12)
+    ]
+
+
+def test_should_draw_offset_from_component_coordinates():
+    icon_size = 10
+    countdown = Countdown(draw=None, config=ImageConfig(),
+                          start=date(2023, 5, 7),
+                          end=date(2023, 5, 10),
+                          moon_phase=MoonPhase.FULL,
+                          icon_size=icon_size, gap=2, max_columns=2)
+
+    spies = [LetterSpy(10) for _ in range(4)]
+    countdown._grid._icons._icons = spies
+    countdown.draw(6, 9)
+
+    assert [spy.last_draw_at for spy in spies] == [
+        (6, 9), (18, 9),
+        (6, 21), (18, 21)
+    ]
---
+++ tests/test_countdown_resize.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+import pytest
+
+from countdown import Appearance, appearance
+
+
+@pytest.mark.parametrize(
+    "days_inclusive,appears",
+    [
+        (15, Appearance(icon_size=20, max_columns=8, gap=4)),
+        (8, Appearance(icon_size=20, max_columns=8, gap=4)),
+        (7, Appearance(icon_size=40, max_columns=7, gap=4)),
+        (4, Appearance(icon_size=40, max_columns=4, gap=4)),
+        (3, Appearance(icon_size=80, max_columns=3, gap=4)),
+    ]
+)
+def test_should_adjust_size_as_uposatha_gets_closer(days_inclusive, appears):
+    assert appearance(date(2023, 1, 1), date(2023, 1, days_inclusive)) == appears
