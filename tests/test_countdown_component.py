9811cf9dbfdf06be14ed051680bf97662e6a0015
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-18 12:59:47 +0800

    Test uses resizer.

--- tests/test_countdown_component.py
+++ tests/test_countdown_component.py
@@ -2,16 +2,22 @@ from datetime import date
 
 from uposatha.elements import MoonPhase
 
-from countdown import Countdown
+from countdown import Countdown, Icons, GridLayout
 from screen import ImageConfig
 
 
 def test_should_report_height_for_two_rows():
-    countdown = Countdown(draw=None, config=ImageConfig(), resizer=None,
+    def resizer(icons: Icons, grid: GridLayout) -> None:
+        icons.icon_size = 10
+        grid._icon_size = 10
+        grid._max_columns = 2
+        grid._gap = 2
+
+    countdown = Countdown(draw=None, config=ImageConfig(), resizer=resizer,
                           start=date(2023, 5, 7),
                           end=date(2023, 5, 10),
                           moon_phase=MoonPhase.FULL,
-                          icon_size=10, gap=2, max_columns=2)
+                          icon_size=0, gap=0, max_columns=0)
 
     assert countdown.height() == 22
 
