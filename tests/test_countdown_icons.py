b7f03310b340c6f14ec748c6329ba853e995d1c1
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-18 09:27:04 +0800

    test_letters was half baked. Rewrote to work with Icons instead of Countdown and actually uses test parameters.

--- tests/test_countdown_icons.py
+++ tests/test_countdown_icons.py
@@ -43,12 +43,8 @@ def test_should_convert_icon_collection_to_string():
     ]
 )
 def test_letters(start, end, seq):
-    countdown = Countdown(draw=None, config=ImageConfig(),
-                          start=date(2010, 3, 26), end=date(2010, 4, 9),
-                          moon_phase=MoonPhase.FULL,
-                          icon_size=0, gap=0, max_columns=0)
-
-    assert str(countdown) == "FSSMTWTFSSMTWT*"
+    icons = Icons(None, ImageConfig(), 0, start, end, MoonPhase.FULL)
+    assert str(icons) == seq
 
 def test_should_raise_error_when_index_too_large():
     icons = Icons(None, ImageConfig(), 1, date(2023, 6, 5), date(2023, 6, 7), MoonPhase.FULL)
