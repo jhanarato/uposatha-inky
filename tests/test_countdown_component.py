d383a62898d5fd2c2fc75adbe1f630822dfcfb91
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-18 13:03:49 +0800

    Tests use resizer.

--- tests/test_countdown_component.py
+++ tests/test_countdown_component.py
@@ -23,30 +23,48 @@ def test_should_report_height_for_two_rows():
 
 
 def test_should_report_width_for_two_rows():
-    countdown = Countdown(draw=None, config=ImageConfig(), resizer=None,
+    def resizer(icons: Icons, grid: GridLayout) -> None:
+        icons.icon_size = 10
+        grid._icon_size = 10
+        grid._max_columns = 2
+        grid._gap = 2
+
+    countdown = Countdown(draw=None, config=ImageConfig(), resizer=resizer,
                           start=date(2023, 5, 7),
                           end=date(2023, 5, 10),
                           moon_phase=MoonPhase.FULL,
-                          icon_size=10, gap=2, max_columns=2)
+                          icon_size=0, gap=0, max_columns=0)
 
     assert countdown.width() == 22
 
 
 def test_should_report_width_for_shorter_first_row():
-    countdown = Countdown(draw=None, config=ImageConfig(), resizer=None,
+    def resizer(icons: Icons, grid: GridLayout) -> None:
+        icons.icon_size = 10
+        grid._icon_size = 10
+        grid._max_columns = 3
+        grid._gap = 2
+
+    countdown = Countdown(draw=None, config=ImageConfig(), resizer=resizer,
                           start=date(2023, 5, 7),
                           end=date(2023, 5, 10),
                           moon_phase=MoonPhase.FULL,
-                          icon_size=10, gap=2, max_columns=3)
+                          icon_size=0, gap=0, max_columns=0)
 
     assert countdown.width() == 34
 
 
 def test_should_report_width_for_single_row():
-    countdown = Countdown(draw=None, config=ImageConfig(), resizer=None,
+    def resizer(icons: Icons, grid: GridLayout) -> None:
+        icons.icon_size = 10
+        grid._icon_size = 10
+        grid._max_columns = 5
+        grid._gap = 2
+
+    countdown = Countdown(draw=None, config=ImageConfig(), resizer=resizer,
                           start=date(2023, 5, 7),
                           end=date(2023, 5, 10),
                           moon_phase=MoonPhase.FULL,
-                          icon_size=10, gap=2, max_columns=5)
+                          icon_size=0, gap=0, max_columns=0)
 
     assert countdown.width() == 46
