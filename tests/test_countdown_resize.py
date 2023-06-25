d5e9f56c73389a47783acfaf3c1ac9f9ccd88839
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-25 10:51:34 +0800

    Removed property & test. The property just existed to satisfy the test and the test is somewhat unnecessary now that the zoomer function just returns a dataclass instead of modifying the Icons and GridLayout instances.
    Issue #7

--- tests/test_countdown_resize.py
+++ tests/test_countdown_resize.py
@@ -26,15 +26,6 @@ def test_should_increase_icon_size_as_uposatha_approaches(days_inclusive, icon_s
     appearance = zoom_on_approach(days_inclusive)
     assert appearance.icon_size == icon_size
 
-def test_should_modify_icon_size_with_resizer():
-    start, end = days(4)
-
-    def resizer(icons: int) -> Appearance:
-        return Appearance(icon_size=3, max_columns=0, gap=0)
-
-    countdown = Countdown(None, ImageConfig(), resizer, start, end, MoonPhase.FULL)
-    assert countdown.icon_size == 3
-
 def test_should_adjust_columns_while_zooming():
     countdown = Countdown(draw=None, config=ImageConfig(), resizer=zoom_on_approach,
                           start=date(2023, 5, 7), end=date(2023, 5, 10),
