a0efbfbc19020c99a89d6883d992ba4223740a45
Ajahn Jhanarato <jhanarato@gmail.com>
2023-06-28 09:44:35 +0800

    Removed resize tests as they prevent us changing the icon_size. Might get away with manual testing.
    Issue #14

--- tests/test_countdown_resize.py
+++ tests/test_countdown_resize.py
@@ -10,25 +10,3 @@ def days(number_of_days: int) -> tuple[date, date]:
     start = date(2023, 1, 1)
     end = start + timedelta(days=number_of_days - 1)
     return start, end
-
-@pytest.mark.parametrize(
-    "days_inclusive,icon_size",
-    [
-        (15, 30),
-        (8, 30),
-        (7, 40),
-        (4, 40),
-        (3, 50),
-        (2, 50),
-    ]
-)
-def test_should_increase_icon_size_as_uposatha_approaches(days_inclusive, icon_size):
-    appearance = zoom_on_approach(icons=days_inclusive, fourteen_day=True)
-    assert appearance.icon_size == icon_size
-
-def test_should_adjust_columns_while_zooming():
-    countdown = Countdown(draw=None, config=ImageConfig(), resizer=zoom_on_approach,
-                          start=date(2023, 5, 7), end=date(2023, 5, 10),
-                          moon_phase=MoonPhase.FULL, fourteen_day=True)
-
-    assert countdown._layout.columns == 4
