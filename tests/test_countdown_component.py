ba500832761c9485d91cfab1d242c3b1dfcb3bd9
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-06 11:01:06 +0800

    Tidied tests.

--- tests/test_countdown_component.py
+++ tests/test_countdown_component.py
@@ -10,8 +10,8 @@ def test_should_report_height_for_two_rows():
     appearances = IconCountMapping[Appearance](14)
     appearances[1, 14] = Appearance(icon_size=10, max_columns=2, gap=2)
 
-    countdown = Countdown(draw=None, config=ImageConfig(), appearances=appearances,
-                          start=date(2023, 5, 7), end=date(2023, 5, 10), moon_phase=MoonPhase.FULL)
+    countdown = Countdown(None, ImageConfig(), appearances,
+                          date(2023, 5, 7), date(2023, 5, 10), MoonPhase.FULL)
 
     assert countdown.height() == 22
 
@@ -19,8 +19,9 @@ def test_should_report_height_for_two_rows():
 def test_should_report_width_for_two_rows():
     appearances = IconCountMapping[Appearance](14)
     appearances[1, 14] = Appearance(icon_size=10, max_columns=2, gap=2)
-    countdown = Countdown(draw=None, config=ImageConfig(), appearances=appearances,
-                          start=date(2023, 5, 7), end=date(2023, 5, 10), moon_phase=MoonPhase.FULL)
+
+    countdown = Countdown(None, ImageConfig(), appearances,
+                          date(2023, 5, 7), date(2023, 5, 10), MoonPhase.FULL)
 
     assert countdown.width() == 22
 
@@ -29,8 +30,8 @@ def test_should_report_width_for_shorter_first_row():
     appearances = IconCountMapping[Appearance](14)
     appearances[1, 14] = Appearance(icon_size=10, max_columns=3, gap=2)
 
-    countdown = Countdown(draw=None, config=ImageConfig(), appearances=appearances,
-                          start=date(2023, 5, 7), end=date(2023, 5, 10), moon_phase=MoonPhase.FULL)
+    countdown = Countdown(None, ImageConfig(), appearances,
+                          date(2023, 5, 7), date(2023, 5, 10), MoonPhase.FULL)
 
     assert countdown.width() == 34
 
@@ -39,7 +40,7 @@ def test_should_report_width_for_single_row():
     appearances = IconCountMapping[Appearance](14)
     appearances[1, 14] = Appearance(icon_size=10, max_columns=5, gap=2)
 
-    countdown = Countdown(draw=None, config=ImageConfig(), appearances=appearances,
-                          start=date(2023, 5, 7), end=date(2023, 5, 10), moon_phase=MoonPhase.FULL)
+    countdown = Countdown(None, ImageConfig(), appearances,
+                          date(2023, 5, 7), date(2023, 5, 10), MoonPhase.FULL)
 
     assert countdown.width() == 46
