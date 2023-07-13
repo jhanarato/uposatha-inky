4fc58e787a26decd90ae4c3c0f780d8b167b27ba
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-13 12:32:50 +0800

    Refactored Countdown creation.

--- src/compose.py
+++ src/compose.py
@@ -17,25 +17,24 @@ MEDIUM_ICON = 40
 LARGE_ICON = 45
 LARGEST_ICON = 80
 
-def new_countdown(config: ImageConfig, today: date, uposatha_falls_on: date,
-                  moon_phase: MoonPhase, fourteen_day: bool) -> Countdown:
+def fifteen_day_appearance():
+    appearances = IconCountMapping[Appearance](15)
+    appearances[11, 15] = Appearance(SMALLEST_ICON, 5, GAP)
+    appearances[8, 10] = Appearance(SMALL_ICON, 5, GAP)
+    appearances[4, 7] = Appearance(MEDIUM_ICON, 8, GAP)
+    appearances[2, 3] = Appearance(LARGE_ICON, 8, GAP)
+    appearances[1] = Appearance(LARGEST_ICON, 8, GAP)
+    return appearances
 
-    if fourteen_day:
-        appearances = IconCountMapping[Appearance](14)
-        appearances[8, 14] = Appearance(SMALL_ICON, 7, GAP)
-        appearances[4, 7] = Appearance(MEDIUM_ICON, 7, GAP)
-        appearances[2, 3] = Appearance(LARGE_ICON, 7, GAP)
-        appearances[1] = Appearance(LARGEST_ICON, 7, GAP)
-    else:
-        appearances = IconCountMapping[Appearance](15)
-        appearances[11, 15] = Appearance(SMALLEST_ICON, 5, GAP)
-        appearances[8, 10] = Appearance(SMALL_ICON, 5, GAP)
-        appearances[4, 7] = Appearance(MEDIUM_ICON, 8, GAP)
-        appearances[2, 3] = Appearance(LARGE_ICON, 8, GAP)
-        appearances[1] = Appearance(LARGEST_ICON, 8, GAP)
 
-    return Countdown(config=config, appearances=appearances,
-                     start=today, end=uposatha_falls_on, moon_phase=moon_phase)
+def fourteen_day_appearance():
+    appearances = IconCountMapping[Appearance](14)
+    appearances[8, 14] = Appearance(SMALL_ICON, 7, GAP)
+    appearances[4, 7] = Appearance(MEDIUM_ICON, 7, GAP)
+    appearances[2, 3] = Appearance(LARGE_ICON, 7, GAP)
+    appearances[1] = Appearance(LARGEST_ICON, 7, GAP)
+    return appearances
+
 
 def next_uposatha(content: NextUposatha) -> Image:
     config = ImageConfig()
@@ -50,11 +49,16 @@ def next_uposatha(content: NextUposatha) -> Image:
 
     draw = ImageDraw.Draw(image)
 
+    if content.fourteen_day:
+        appearance = fourteen_day_appearance()
+    else:
+        appearance = fifteen_day_appearance()
+
     components = [
         Text("Uposatha", font_styles.HEADING, palette.BLACK),
         HorizontalLine(length=300, thickness=2, colour=palette.BLACK),
         Text(content.date, font_styles.INFO, palette.BLACK),
-        new_countdown(config, content.today, content.falls_on, content.moon_phase, content.fourteen_day),
+        Countdown(config, appearance, content.today, content.falls_on, content.moon_phase),
         Text(content.details, font_styles.INFO, palette.BLACK),
     ]
 
