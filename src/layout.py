4109a7ef3fa8e9179029d93d4191a4e10fa999c3
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-13 10:47:56 +0800

    Coordinates stored, tests pass!
    Issue #19

--- src/layout.py
+++ src/layout.py
@@ -31,34 +31,26 @@ class ScreenLayout:
     def __init__(self, screen_height: int, screen_width: int):
         self._screen_height = screen_height
         self._screen_width = screen_width
-        self._arrangement: list[tuple[Area, Align]] = []
+        self._y = 0
+        self._coordinates = []
 
     def add_space(self, height: int) -> None:
-        self._arrangement.append((VerticalSpace(height=height), Align.CENTRE))
+        self._y += height
 
     def add_left(self, component: Area) -> None:
-        self._arrangement.append((component, Align.LEFT))
+        x = 0
+        self._coordinates.append((x, self._y))
+        self._y += component.height()
 
     def add_right(self, component: Area) -> None:
-        self._arrangement.append((component, Align.RIGHT))
+        x = self._screen_width - component.width()
+        self._coordinates.append((x, self._y))
+        self._y += component.height()
 
     def add_centred(self, component: Area) -> None:
-        self._arrangement.append((component, Align.CENTRE))
-
-    def _align_x(self, area: Area, align: Align) -> int:
-        x = 0
-        if align == Align.LEFT:
-            x = 0
-        elif align == Align.CENTRE:
-            x = (self._screen_width - area.width()) // 2
-        elif align == Align.RIGHT:
-            x = self._screen_width - area.width()
-        return x
+        x = (self._screen_width - component.width()) // 2
+        self._coordinates.append((x, self._y))
+        self._y += component.height()
 
     def coordinates(self) -> Iterator[tuple[int, int]]:
-        y = 0
-        for area, align in self._arrangement:
-            x = self._align_x(area, align)
-            if not isinstance(area, VerticalSpace):
-                yield x, y
-            y += area.height()
+        return iter(self._coordinates)
