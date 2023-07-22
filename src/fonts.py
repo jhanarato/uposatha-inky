9d08001308288cb95375d195a3127bbac79a4361
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-22 10:23:32 +0800

    Implemented pixel_bbox(). Have to test, not sure if it works.
    Issue #21

--- src/fonts.py
+++ src/fonts.py
@@ -1,4 +1,6 @@
+import itertools
 from dataclasses import dataclass
+from collections.abc import Iterator
 
 from PIL import Image, ImageFont, ImageDraw
 from font_roboto import RobotoBold
@@ -37,9 +39,41 @@ def font_bbox(text: str, font: ImageFont) -> BBox:
     return BBox(*bbox)
 
 def image_bbox(text: str, font: ImageFont) -> BBox:
-    image = Image.new(mode="P", size=(100, 100), color=0)
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), text, 1, font)
+    image = text_image(text, font)
     bbox = image.getbbox()
     return BBox(*bbox)
 
+def pixel_bbox(text: str, font: ImageFont) -> BBox:
+    image = text_image(text, font)
+    pixels = black_pixels(image)
+    return pixels_to_bbox(pixels)
+
+def text_image(text, font):
+    image = Image.new(mode="P", size=(100, 100), color=0)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), text, 1, font)
+    return image
+
+def black_pixels(image: Image) -> Iterator[tuple[int, int]]:
+    pixels = image.load()
+    columns = image.size[0]
+    rows = image.size[1]
+
+    for column, row in itertools.product(range(columns), range(rows)):
+        if pixels[column, row] == 1:
+            yield column, row
+
+def pixels_to_bbox(pixels: Iterator[tuple[int, int]]) -> BBox:
+    column, row = next(pixels)
+    left = column
+    right = column
+    top = row
+    bottom = row
+
+    for column, row in pixels:
+        left = min(left, column)
+        right = max(right, column)
+        top = min(top, row)
+        bottom = min(bottom, row)
+
+    return BBox(left=left, right=right, top=top, bottom=bottom)
