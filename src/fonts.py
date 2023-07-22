433661b7fdf2f9ba484a5926ead0a426915630a8
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-22 09:54:44 +0800

    fonts module has two functions to find the bbox of text.
    Issue #21

--- src/fonts.py
+++ src/fonts.py
@@ -1,4 +1,6 @@
-from PIL import ImageFont
+from dataclasses import dataclass
+
+from PIL import Image, ImageFont, ImageDraw
 from font_roboto import RobotoBold
 
 
@@ -22,3 +24,22 @@ class Font:
 
     def pil_font(self) -> ImageFont:
         return self._font
+
+@dataclass
+class BBox:
+    left: int
+    top: int
+    right: int
+    bottom: int
+
+def font_bbox(text: str, font: ImageFont) -> BBox:
+    bbox = font.getbbox(text)
+    return BBox(*bbox)
+
+def image_bbox(text: str, font: ImageFont) -> BBox:
+    image = Image.new(mode="P", size=(100, 100), color=0)
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), text, 1, font)
+    bbox = image.getbbox()
+    return BBox(*bbox)
+
--- tests/test_fonts.py
+++ tests/test_fonts.py
@@ -1,6 +1,10 @@
 import pytest
 
-from fonts import Font
+from PIL import ImageFont
+from font_roboto import RobotoBold
+
+from fonts import Font, image_bbox, BBox, font_bbox
+
 
 @pytest.fixture
 def font():
@@ -17,3 +21,17 @@ def test_should_measure_height_of_text(font):
 
 def test_should_measure_width_of_text(font):
     assert font.width("Hello") == 70
+
+def test_should_make_image_bbox(font):
+    bbox = image_bbox("H", ImageFont.truetype(RobotoBold, 30))
+    assert bbox.left == 2
+    assert bbox.top == 7
+    assert bbox.right == 19
+    assert bbox.bottom == 28
+
+def test_should_make_font_bbox(font):
+    bbox = font_bbox("H", ImageFont.truetype(RobotoBold, 30))
+    assert bbox.left == 0
+    assert bbox.top == 7
+    assert bbox.right == 21
+    assert bbox.bottom == 28
