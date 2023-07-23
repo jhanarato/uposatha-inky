ca19abc282dc5dd09b484ed7f7a4bb5ea0c88cff
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-23 09:43:51 +0800

    Added tests that confirm the baseline is maintained as long as the text is drawn at the same y coordinate.
    Issue #9

--- tests/test_fonts.py
+++ tests/test_fonts.py
@@ -17,10 +17,10 @@ def test_should_have_style(font):
     assert font.style == "Bold"
 
 def test_should_measure_height_of_text(font):
-    assert font.height("Hello") == 28
+    assert font.height("H") == 28
 
 def test_should_measure_width_of_text(font):
-    assert font.width("Hello") == 70
+    assert font.width("H") == 70
 
 def test_should_make_image_bbox(font):
     bbox = image_bbox("H", ImageFont.truetype(RobotoBold, 30))
