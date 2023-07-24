67230090a1a21abdc1ed4f999595a333e7c8c60a
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-24 17:40:47 +0800

    Changed camel case to snake case.

--- Experiments/witdh_finder.py
+++ Experiments/witdh_finder.py
@@ -10,19 +10,19 @@ t = cmap.getcmap(3,1).cmap
 s = font.getGlyphSet()
 units_per_em = font['head'].unitsPerEm
 
-def getTextWidth(text,pointSize):
+def get_text_width(text, point_size):
     total = 0
     for c in text:
         if ord(c) in t and t[ord(c)] in s:
             total += s[t[ord(c)]].width
         else:
             total += s['.notdef'].width
-    total = total*float(pointSize)/units_per_em;
+    total = total * float(point_size) / units_per_em;
     return total
 
 text = 'This is a test'
 
-width = getTextWidth(text,12)
+width = get_text_width(text, 12)
 
 print ('Text: "%s"' % text)
 print ('Width in points: %f' % width)
