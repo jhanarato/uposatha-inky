4f25b4f5a9ec0a5e3e31bc8b08c5552bb457c4fd
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-24 17:46:13 +0800

    Comment.

--- Experiments/test_width_finder.py
+++ Experiments/test_width_finder.py
@@ -1,6 +1,6 @@
 from Experiments.witdh_finder import get_text_width
 
-
+# Approval test for refactoring.
 def test_width_finder():
     text = 'This is a test'
     assert get_text_width(text, 12) == 69.029296875
