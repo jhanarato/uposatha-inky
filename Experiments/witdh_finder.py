c0036c207171d406d181481ac051c5cd9ad85e69
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-25 17:23:13 +0800

    Moved glyph specific stuff to glyph()

--- Experiments/witdh_finder.py
+++ Experiments/witdh_finder.py
@@ -4,9 +4,7 @@
 from fontTools.ttLib import TTFont
 
 def get_text_width(text: str, font: TTFont, point_size: int):
-    character_map = font['cmap'].getcmap(3, 1).cmap
-    glyph_set = font.getGlyphSet()
-    widths = [code_point_width(ord(c), character_map, glyph_set) for c in text]
+    widths = [code_point_width(ord(c), font) for c in text]
     total_width = sum(widths)
     return width_in_pixels(total_width, font, point_size)
 
@@ -14,12 +12,16 @@ def width_in_pixels(width: int, font: TTFont, point_size: int) -> float:
     units_per_em = font['head'].unitsPerEm
     return width * point_size / units_per_em
 
-def glyph(code_point, character_map, glyph_set):
+def glyph(code_point: int, font: TTFont):
+    character_map = font['cmap'].getcmap(3, 1).cmap
+    glyph_set = font.getGlyphSet()
+
     if code_point not in character_map:
         return None
     if character_map[code_point] not in glyph_set:
         return None
+
     return glyph_set[character_map[code_point]]
 
-def code_point_width(code_point, character_map, glyph_set):
-    return glyph(code_point, character_map, glyph_set).width
+def code_point_width(code_point, font):
+    return glyph(code_point, font).width
