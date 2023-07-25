be631e89cd5363f8236623ddc70d6090f71e1ed8
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-25 13:08:45 +0800

    Extract method: now we have a function to calculate th width of a single code_point unicode value.

--- Experiments/witdh_finder.py
+++ Experiments/witdh_finder.py
@@ -5,20 +5,24 @@ from fontTools.ttLib import TTFont
 
 def get_text_width(text: str, font: TTFont, point_size: int):
     units_per_em = font['head'].unitsPerEm
+    character_map = font['cmap'].getcmap(3, 1).cmap
+    glyph_set = font.getGlyphSet()
 
     total = 0
 
     for c in text:
         code_point = ord(c)
 
-        character_map = font['cmap'].getcmap(3, 1).cmap
-        glyph_set = font.getGlyphSet()
-
-        if code_point in character_map and character_map[code_point] in glyph_set:
-            point_width = glyph_set[character_map[code_point]].width
-        else:
-            point_width = glyph_set['.notdef'].width
+        point_width = code_point_width(code_point, character_map, glyph_set)
 
         total += point_width
     total = total * (point_size / units_per_em)
     return total
+
+
+def code_point_width(code_point, character_map, glyph_set):
+    if code_point in character_map and character_map[code_point] in glyph_set:
+        point_width = glyph_set[character_map[code_point]].width
+    else:
+        point_width = glyph_set['.notdef'].width
+    return point_width
