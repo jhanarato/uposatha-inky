a74dd14021fe307904e367153594dba11f851bd7
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-25 17:25:55 +0800

    Renamed param. Hopefully "points" is the right term.

--- Experiments/witdh_finder.py
+++ Experiments/witdh_finder.py
@@ -3,14 +3,14 @@
 
 from fontTools.ttLib import TTFont
 
-def get_text_width(text: str, font: TTFont, point_size: int):
+def get_text_width(text: str, font: TTFont, points: int):
     widths = [code_point_width(ord(c), font) for c in text]
     total_width = sum(widths)
-    return width_in_pixels(total_width, font, point_size)
+    return width_in_pixels(total_width, font, points)
 
-def width_in_pixels(width: int, font: TTFont, point_size: int) -> float:
+def width_in_pixels(width: int, font: TTFont, points: int) -> float:
     units_per_em = font['head'].unitsPerEm
-    return width * point_size / units_per_em
+    return width * points / units_per_em
 
 def glyph(code_point: int, font: TTFont):
     character_map = font['cmap'].getcmap(3, 1).cmap
