4aeebfb4c69000d5b4de1c2ea53896d5ee3d8809
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-25 16:57:27 +0800

    Added test showing result is equivalent to Pillow, with a very slight difference.

--- Experiments/test_width_finder.py
+++ Experiments/test_width_finder.py
@@ -1,5 +1,8 @@
+import pytest
+
 from font_roboto import RobotoBold
 from fontTools.ttLib import TTFont
+from PIL import ImageFont
 
 from Experiments.witdh_finder import get_text_width
 
@@ -8,3 +11,15 @@ def test_width_finder():
     text = 'This is a test'
     font = TTFont(RobotoBold)
     assert get_text_width(text, font, 12) == 69.029296875
+
+def test_width_finder_equivalent_to_pillow_length():
+    size = 30
+    text = "H"
+
+    ft_font = TTFont(RobotoBold)
+    pil_font = ImageFont.truetype(RobotoBold, size)
+
+    ft_width = get_text_width(text, ft_font, size)
+    pil_width = pil_font.getlength(text)
+
+    assert ft_width == pytest.approx(pil_width, 0.001)
