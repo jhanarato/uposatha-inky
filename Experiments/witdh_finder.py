4ed6d9473f4d7fbf7c296aab74da6b1611aee7fb
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-25 12:44:53 +0800

    Tidy: make names meaningful.

--- Experiments/witdh_finder.py
+++ Experiments/witdh_finder.py
@@ -5,18 +5,18 @@ from fontTools.ttLib import TTFont
 from font_roboto import RobotoBold
 
 font = TTFont(RobotoBold)
-cmap = font['cmap']
-t = cmap.getcmap(3, 1).cmap
-s = font.getGlyphSet()
+
+character_map = font['cmap'].getcmap(3, 1).cmap
+glyph_set = font.getGlyphSet()
 units_per_em = font['head'].unitsPerEm
 
 def get_text_width(text: str, point_size: int):
     total = 0
     for c in text:
         code_point = ord(c)
-        if code_point in t and t[code_point] in s:
-            total += s[t[code_point]].width
+        if code_point in character_map and character_map[code_point] in glyph_set:
+            total += glyph_set[character_map[code_point]].width
         else:
-            total += s['.notdef'].width
+            total += glyph_set['.notdef'].width
     total = total * float(point_size) / units_per_em
     return total
