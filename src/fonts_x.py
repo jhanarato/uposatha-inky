b626192d88ee18a4795e41b2e452114e9218560d
Ajahn Jhanarato <jhanarato@gmail.com>
2023-07-26 16:43:08 +0800

    Removed functions replaced by Glyph class.

--- src/fonts_x.py
+++ src/fonts_x.py
@@ -3,20 +3,8 @@
 from fontTools.ttLib import TTFont
 
 def text_width_in_points(text: str, font: TTFont, font_points_per_em: int) -> float:
-    return sum([glyph_width_in_points(ord(c), font, font_points_per_em) for c in text])
-
-def glyph_width_in_points(code: int, font: TTFont, font_points_per_em: int) -> float:
-    glyph = Glyph(font, font_points_per_em, code)
-    return glyph.width_in_points
-
-def glyph_width_in_em(code: int, font: TTFont) -> float:
-    return glyph_width_in_units(code, font) / units_per_em(font)
-
-def glyph_width_in_units(code: int, font: TTFont) -> int:
-    return glyph(code, font).width
-
-def units_per_em(font: TTFont) -> int:
-    return font['head'].unitsPerEm
+    glyphs = [Glyph(font, font_points_per_em, ord(c)) for c in text]
+    return sum([glyph.width_in_points for glyph in glyphs])
 
 def glyph(code: int, font: TTFont):
     character_map = font['cmap'].getcmap(3, 1).cmap
