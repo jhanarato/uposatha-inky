f912afbcf2f4a8e14af028910eb69933dc8570ab
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-03 13:05:44 +0800

    Demo draws an icon for each day of week.

--- Experiments/icon_size_demo.py
+++ Experiments/icon_size_demo.py
@@ -1,16 +1,28 @@
 from PIL import Image, ImageDraw
 
 from components import DayOfWeekIcon
-from compose import SMALLEST_ICON
+from compose import SMALLEST_ICON, SMALL_ICON, MEDIUM_ICON, LARGE_ICON, LARGEST_ICON, GAP
 
 
-def display(icon: DayOfWeekIcon):
-    border = 10
-    image = Image.new(mode="P", size=(icon.width() + border * 2, icon.height() + border * 2), color=0)
+def days(size: int) -> list[DayOfWeekIcon]:
+    return [DayOfWeekIcon(letter, size) for letter in "SMTWF"]
+
+def draw_icons(draw: ImageDraw):
+    letters = "SMTWF"
+    sizes = [SMALLEST_ICON, SMALL_ICON, MEDIUM_ICON, LARGE_ICON, LARGEST_ICON]
+    draw_row(draw, letters, sizes[0])
+
+def draw_row(draw: ImageDraw, letters: str, size: int, y: int = 0):
+    x = 0
+    for letter in letters:
+        icon = DayOfWeekIcon(letter, size)
+        icon.draw(draw, x, y)
+        x += size + GAP
+
+def main():
+    image = Image.new(mode="P", size=(500, 500), color=0)
     draw = ImageDraw.Draw(image)
-
-    icon.draw(draw, border, border)
-
+    draw_icons(draw)
     palette = [
         255, 255, 255,  # 0 = WHITE
         0, 0, 0,  # 1 = BLACK
@@ -20,4 +32,5 @@ def display(icon: DayOfWeekIcon):
     converted = image.convert(mode="RGB")
     converted.show()
 
-display(DayOfWeekIcon("S", SMALLEST_ICON))
+if __name__ == "__main__":
+    main()
