9b685bf8f642fbdf236bb4b347a2b09e1e350987
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-05 09:56:46 +0800

    New demo to assess icons in a grid.

---
+++ src/icon_grid_demo.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw
+
+from components import DayOfWeekIcon
+
+def image_width() -> int:
+    return 100
+
+def image_height() -> int:
+    return 100
+
+def draw_icons(draw: ImageDraw) -> None:
+    icon = DayOfWeekIcon("S", 30)
+    icon.draw(draw, 35, 35)
+
+def main():
+    width = image_width()
+    height = image_height()
+
+    image = Image.new(mode="P", size=(width, height), color=0)
+    draw = ImageDraw.Draw(image)
+
+    draw_icons(draw)
+
+    palette = [
+        255, 255, 255,  # 0 = WHITE
+        0, 0, 0,  # 1 = BLACK
+        255, 255, 0  # 2 = YELLOW
+    ]
+
+    image.putpalette(palette)
+    converted = image.convert(mode="RGB")
+    converted.show()
+
+if __name__ == "__main__":
+    main()
