6f4280c72f925e068408b6134025dc46cb2958eb
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-05 09:32:24 +0800

    Added whitespace.

--- src/icon_size_demo.py
+++ src/icon_size_demo.py
@@ -47,16 +47,21 @@ def main():
     border = 20
     letters = "SMTWF"
     sizes = [SMALLEST_ICON, SMALL_ICON, MEDIUM_ICON, LARGE_ICON, LARGEST_ICON]
+
     width = image_width(border, letters, sizes)
     height = image_height(border, sizes)
+
     image = Image.new(mode="P", size=(width, height), color=0)
     draw = ImageDraw.Draw(image)
+
     draw_icons(draw, border, letters, sizes)
+
     palette = [
         255, 255, 255,  # 0 = WHITE
         0, 0, 0,  # 1 = BLACK
         255, 255, 0  # 2 = YELLOW
     ]
+
     image.putpalette(palette)
     converted = image.convert(mode="RGB")
     converted.show()
