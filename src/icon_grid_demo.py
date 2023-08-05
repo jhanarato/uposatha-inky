d4abb4617ef17d062aaa37d0bdd43ae3a42c5859
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-05 13:33:53 +0800

    grid coordinates and image size.

--- src/icon_grid_demo.py
+++ src/icon_grid_demo.py
@@ -10,14 +10,21 @@ ICON_SIZE = 10
 BORDER = 20
 GAP = 2
 
-def image_width() -> int:
-    return 100
-
-def image_height() -> int:
-    return 100
+def image_size(letter_count: int) -> int:
+    icon_space = ICON_SIZE * letter_count
+    gap_space = GAP * (letter_count - 1)
+    borders = 2 * BORDER
+    return icon_space + gap_space + borders
 
 def axis_coordinates(position: int) -> list[int]:
     return [(ICON_SIZE + GAP) * column for column in range(position)]
+
+def grid_coordinates(grid_size: int) -> Iterator[tuple[int, int]]:
+    for row in range(grid_size):
+        for column in range(grid_size):
+            x = BORDER + ((ICON_SIZE + GAP) * column)
+            y = BORDER + ((ICON_SIZE + GAP) * row)
+            yield x, y
 
 def shifted_grid(letters: str) -> Iterator[DayOfWeekIcon]:
     icons = deque(icon_row(letters))
@@ -33,10 +40,10 @@ def draw_icons(draw: ImageDraw) -> None:
     icon.draw(draw, 35, 35)
 
 def main():
-    width = image_width()
-    height = image_height()
+    letters = "SMTWF"
+    size = image_size(len(letters))
 
-    image = Image.new(mode="P", size=(width, height), color=0)
+    image = Image.new(mode="P", size=(size, size), color=0)
     draw = ImageDraw.Draw(image)
 
     draw_icons(draw)
--- tests/test_icon_grid_demo.py
+++ tests/test_icon_grid_demo.py
@@ -1,4 +1,4 @@
-from icon_grid_demo import icon_row, shifted_grid, axis_coordinates
+from icon_grid_demo import icon_row, shifted_grid, grid_coordinates, image_size
 
 
 def test_shifted_grid():
@@ -11,5 +11,12 @@ def test_icon_rows():
     result = "".join([icon.letter for icon in row])
     assert result == "ABC"
 
-def test_axis_coordinates():
-    assert axis_coordinates(position=3) == [0, 12, 24]
+def test_grid_coordinates():
+    assert list(grid_coordinates(3)) == [
+        (20, 20), (32, 20), (44, 20),
+        (20, 32), (32, 32), (44, 32),
+        (20, 44), (32, 44), (44, 44)
+    ]
+
+def test_image_size():
+    assert image_size(letter_count=3) == 74
