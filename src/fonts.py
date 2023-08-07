d7edc5faba0c284c0dcfc3247fd65473659ec580
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-07 12:22:10 +0800

    Glyph and DesignUnits moved to fonts.py font_x.py is no longer.

--- src/fonts.py
+++ src/fonts.py
@@ -2,8 +2,41 @@ import itertools
 from dataclasses import dataclass
 from collections.abc import Iterator
 
+import glyphtools
 from PIL import Image, ImageFont, ImageDraw
+from fontTools.ttLib import TTFont
 from font_roboto import RobotoBold
+
+
+class DesignUnits:
+    def __init__(self, units: int, units_per_em: int):
+        self._units = units
+        self._units_per_em = units_per_em
+
+    def units(self) -> int:
+        return self._units
+
+    def to_em(self) -> float:
+        return self.units() / self._units_per_em
+
+    def to_points(self, font_size: int) -> float:
+        return self.to_em() * font_size
+
+
+class Glyph:
+    def __init__(self, font: TTFont, char: str):
+        self._font = font
+        self._char = char
+        self._metrics = glyphtools.get_glyph_metrics(font, char)
+
+    def _units_per_em(self) -> int:
+        return self._font['head'].unitsPerEm
+
+    def width(self) -> DesignUnits:
+        return DesignUnits(self._metrics["width"], self._units_per_em())
+
+    def left_side_bearing(self) -> DesignUnits:
+        return DesignUnits(self._metrics["lsb"], self._units_per_em())
 
 
 class Font:
--- tests/test_fonts.py
+++ tests/test_fonts.py
@@ -1,9 +1,11 @@
 import pytest
+
 from PIL import Image, ImageFont
+from fontTools.ttLib import TTFont
 from font_roboto import RobotoBold
 
 from fonts import Font, image_bbox, font_bbox, black_pixels, pixels_to_bbox, pixel_bbox, BBox
-
+from fonts import DesignUnits, Glyph
 
 @pytest.fixture
 def font():
@@ -95,3 +97,49 @@ def test_should_be_below_baseline_when_descent():
     above = pixel_bbox("H", font).bottom
     below = pixel_bbox("y", font).bottom
     assert below > above
+
+
+@pytest.fixture
+def glyph():
+    font = TTFont(RobotoBold)
+    return Glyph(font, "H")
+
+
+def test_glyph_width_in_units(glyph):
+    assert glyph.width().units() == 1448
+
+
+def test_glyph_width_in_em(glyph):
+    assert glyph.width().to_em() == 0.70703125
+
+
+def test_glyph_width_in_points(glyph):
+    assert glyph.width().to_points(font_size=16) == 11.3125
+
+
+def test_glyph_left_side_bearing(glyph):
+    assert glyph.left_side_bearing().units() == 130
+
+
+def test_design_units_available():
+    units = DesignUnits(units=100, units_per_em=2000)
+    assert units.units() == 100
+
+
+def test_design_units_converts_to_em():
+    units = DesignUnits(units=100, units_per_em=2000)
+    assert units.to_em() == 0.05
+
+
+def test_design_converts_font_points_to_glyph_points():
+    units = DesignUnits(units=100, units_per_em=2000)
+    assert units.to_points(font_size=10) == 0.5
+
+
+def test_glyph_with_space():
+    with pytest.raises(KeyError):
+        glyph = Glyph(TTFont(RobotoBold), " ")
+
+
+def test_lsb_as_points(glyph):
+    assert glyph.left_side_bearing().to_points(font_size=30) == 1.904296875
