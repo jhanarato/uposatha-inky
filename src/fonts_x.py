8a3e2ede7b736d0a5573434d6945ccc365fe9c03
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-07 12:13:56 +0800

    Removed cool but not-needed function. I did add it to the project wiki for future reference.

--- src/fonts_x.py
+++ src/fonts_x.py
@@ -33,18 +33,3 @@ class Glyph:
 
     def left_side_bearing(self) -> DesignUnits:
         return DesignUnits(self._metrics["lsb"], self._units_per_em())
-
-
-def get_glyph(code: int, font: TTFont):
-    character_map = font['cmap'].getcmap(3, 1).cmap
-    glyph_set = font.getGlyphSet()
-
-    if code not in character_map:
-        raise RuntimeError(f"Code {code} not in character map")
-
-    character = character_map[code]
-
-    if character not in glyph_set:
-        raise RuntimeError(f"Character {character} not in glyph set.")
-
-    return glyph_set[character]
