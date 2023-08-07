21dc7035e78e3d48145d52d041053a8ecbee8f82
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-07 10:50:29 +0800

    Reformatted code.

--- tests/test_fonts.py
+++ tests/test_fonts.py
@@ -1,5 +1,4 @@
 import pytest
-
 from PIL import Image, ImageFont
 from font_roboto import RobotoBold
 
@@ -10,25 +9,32 @@ from fonts import Font, image_bbox, font_bbox, black_pixels, pixels_to_bbox, pix
 def font():
     return Font(size=30)
 
+
 def test_should_have_family(font):
     assert font.family == "Roboto"
+
 
 def test_should_have_style(font):
     assert font.style == "Bold"
 
+
 def test_should_measure_height_of_text(font):
     assert font.height("Hello") == 28
 
+
 def test_should_measure_width_of_text(font):
     assert font.width("Hello") == 70
+
 
 def test_should_calculate_height_of_bbox():
     bbox = BBox(top=5, bottom=9, left=0, right=0)
     assert bbox.height == 5
 
+
 def test_should_calculate_width_of_bbox():
     bbox = BBox(left=7, right=12, top=0, bottom=0)
     assert bbox.width == 6
+
 
 def test_should_make_image_bbox(font):
     bbox = image_bbox("H", ImageFont.truetype(RobotoBold, 30))
@@ -37,6 +43,7 @@ def test_should_make_image_bbox(font):
     assert bbox.right == 18
     assert bbox.bottom == 27
 
+
 def test_should_make_font_bbox(font):
     bbox = font_bbox("H", ImageFont.truetype(RobotoBold, 30))
     assert bbox.left == 0
@@ -44,12 +51,14 @@ def test_should_make_font_bbox(font):
     assert bbox.right == 21
     assert bbox.bottom == 28
 
+
 def test_should_get_black_pixel_coordinates(font):
     image = Image.new(mode="P", size=(100, 100), color=0)
     pixels = image.load()
     pixels[13, 17] = 1
     pixels[55, 77] = 1
     assert list(black_pixels(image)) == [(13, 17), (55, 77)]
+
 
 def test_should_convert_pixels_to_bounding_box(font):
     image = Image.new(mode="P", size=(100, 100), color=0)
@@ -62,6 +71,7 @@ def test_should_convert_pixels_to_bounding_box(font):
     assert bbox.right == 55
     assert bbox.bottom == 77
 
+
 def test_should_adjust_image_bbox_to_match_pixel_bbox():
     text = "Hello"
     font = ImageFont.truetype(RobotoBold, 30)
@@ -72,11 +82,13 @@ def test_should_adjust_image_bbox_to_match_pixel_bbox():
     assert pixel.right == image.right
     assert pixel.bottom == image.bottom
 
+
 def test_should_align_text_baseline_when_no_descent():
     text = "Hello"
     font = ImageFont.truetype(RobotoBold, 30)
     bottoms = [pixel_bbox(c, font).bottom for c in text]
     assert len(set(bottoms)) == 1
+
 
 def test_should_be_below_baseline_when_descent():
     font = ImageFont.truetype(RobotoBold, 30)
--- tests/test_fonts_x.py
+++ tests/test_fonts_x.py
@@ -1,7 +1,6 @@
 import pytest
-
-from font_roboto import RobotoBold
 from fontTools.ttLib import TTFont
+from font_roboto import RobotoBold
 
 from fonts_x import Glyph, DesignUnits
 
@@ -11,33 +10,42 @@ def glyph():
     font = TTFont(RobotoBold)
     return Glyph(font, "H")
 
+
 def test_glyph_width_in_units(glyph):
     assert glyph.width().units() == 1448
+
 
 def test_glyph_width_in_em(glyph):
     assert glyph.width().to_em() == 0.70703125
 
+
 def test_glyph_width_in_points(glyph):
     assert glyph.width().to_points(font_size=16) == 11.3125
 
+
 def test_glyph_left_side_bearing(glyph):
     assert glyph.left_side_bearing().units() == 130
+
 
 def test_design_units_available():
     units = DesignUnits(units=100, units_per_em=2000)
     assert units.units() == 100
 
+
 def test_design_units_converts_to_em():
     units = DesignUnits(units=100, units_per_em=2000)
     assert units.to_em() == 0.05
+
 
 def test_design_converts_font_points_to_glyph_points():
     units = DesignUnits(units=100, units_per_em=2000)
     assert units.to_points(font_size=10) == 0.5
 
+
 def test_glyph_with_space():
     with pytest.raises(KeyError):
         glyph = Glyph(TTFont(RobotoBold), " ")
+
 
 def test_lsb_as_points(glyph):
     assert glyph.left_side_bearing().to_points(font_size=30) == 1.904296875
