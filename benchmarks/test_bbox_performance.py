87e4f43b041bcdf1b9819cf5dfb0999f90004cfe
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-13 17:12:33 +0800

    Added blank lines

--- benchmarks/test_bbox_performance.py
+++ benchmarks/test_bbox_performance.py
@@ -7,8 +7,10 @@ from bbox import font_bbox, image_bbox, pixel_bbox
 def test_font_bbox(benchmark):
     benchmark(font_bbox, 'H', ImageFont.truetype(RobotoBold, 30))
 
+
 def test_image_bbox(benchmark):
     benchmark(image_bbox, 'H', ImageFont.truetype(RobotoBold, 30))
+
 
 def test_pixel_bbox(benchmark):
     benchmark(pixel_bbox, 'H', ImageFont.truetype(RobotoBold, 30))
