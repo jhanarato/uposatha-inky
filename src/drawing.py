88c7b6b920de675a92661fe544d1f6688d916608
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-13 16:28:54 +0800

    Created a context manager for creating and showing images.

---
+++ src/drawing.py
@@ -0,0 +1,29 @@
+from PIL import Image, ImageDraw
+
+from screen import Ink
+
+
+class Drawing:
+    def __init__(self, height: int, width: int):
+        self._height = height
+        self._width = width
+
+    def __enter__(self) -> ImageDraw:
+        self._image = Image.new(
+            mode="P",
+            size=(self._width, self._height),
+            color=Ink.WHITE.value
+        )
+        return ImageDraw.Draw(self._image)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not exc_type:
+            palette = [
+                255, 255, 255,  # 0 = WHITE
+                0, 0, 0,  # 1 = BLACK
+                255, 255, 0  # 2 = YELLOW
+            ]
+
+            self._image.putpalette(palette)
+            converted = self._image.convert(mode="RGB")
+            converted.show()
