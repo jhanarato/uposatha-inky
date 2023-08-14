3f3a45c33485d8ee81232242b71ddba2904b9397
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-14 12:54:53 +0800

    Glyph circled.

--- src/glyph_demo.py
+++ src/glyph_demo.py
@@ -1,29 +1,42 @@
 from PIL import ImageDraw
 
-from components import Glyph
+from components import Glyph, Circle
 from screen import Ink
 from viewer import DrawingViewer
 
+HEIGHT = 100
+WIDTH = 100
+
 
 def v_line(draw: ImageDraw, x: int) -> None:
-    draw.line([x, 0, x, 100], width=1, fill=Ink.BLACK.value)
+    draw.line([x, 0, x, HEIGHT], width=1, fill=Ink.BLACK.value)
 
 
 def h_line(draw: ImageDraw, y: int) -> None:
-    draw.line([0, y, 100, y], width=1, fill=Ink.BLACK.value)
+    draw.line([0, y, WIDTH, y], width=1, fill=Ink.BLACK.value)
 
 
 def draw_glyph_in_frame():
-    with DrawingViewer(100, 100) as drawing:
-        glyph = Glyph("H", 60, Ink.BLACK)
-        x = 50 - (glyph.width() // 2)
-        y = 50 - (glyph.height() // 2)
-        glyph.draw(drawing, x, y)
+    with DrawingViewer(height=HEIGHT, width=WIDTH) as drawing:
+        center_x = WIDTH // 2
+        center_y = HEIGHT // 2
 
-        v_line(drawing, x)
-        v_line(drawing, x + glyph.width())
-        h_line(drawing, y)
-        h_line(drawing, y + glyph.height())
+        circle = Circle(80, fill=Ink.WHITE, outline=Ink.BLACK)
+        circle_x = center_x - (circle.width() // 2)
+        circle_y = center_y - (circle.height() // 2)
+
+        circle.draw(drawing, circle_x, circle_y)
+
+        glyph = Glyph("H", 60, Ink.BLACK)
+        glyph_x = center_x - (glyph.width() // 2)
+        glyph_y = center_y - (glyph.height() // 2)
+
+        glyph.draw(drawing, glyph_x, glyph_y)
+
+        v_line(drawing, glyph_x)
+        v_line(drawing, glyph_x + glyph.width())
+        h_line(drawing, glyph_y)
+        h_line(drawing, glyph_y + glyph.height())
 
 
 if __name__ == "__main__":
