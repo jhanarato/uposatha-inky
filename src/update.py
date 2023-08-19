589e0057b23ddfc5b78d5c80a85800aa5b0c453b
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-19 10:16:22 +0800

    update uses context.

--- src/update.py
+++ src/update.py
@@ -1,9 +1,10 @@
 from datetime import date
 
-from content import next_uposatha_content
+from content import get_context, next_uposatha_content
 from compose import next_uposatha
 
 
 def update(today: date):
-    content = next_uposatha_content(today)
+    context = get_context(today)
+    content = next_uposatha_content(context.today)
     next_uposatha(content)
