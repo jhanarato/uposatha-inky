1b082c9d1c48efe8252d4b41cef1b600a518c6cf
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-23 10:27:32 +0800

    Refactored DrawingViewer: creates Image object in __init__ rather than __enter__

--- src/viewer.py
+++ src/viewer.py
@@ -6,15 +6,9 @@ from screen import Ink
 
 class DrawingViewer:
     def __init__(self, height: int, width: int):
-        self._height = height
-        self._width = width
+        self._image = Image.new(mode="P", size=(width, height), color=Ink.WHITE.value)
 
     def __enter__(self) -> ImageDraw:
-        self._image = Image.new(
-            mode="P",
-            size=(self._width, self._height),
-            color=Ink.WHITE.value
-        )
         return ImageDraw.Draw(self._image)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
