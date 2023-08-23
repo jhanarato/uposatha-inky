7e4057d1d52f28745c1a0b3cfa75e4bdc780b268
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-23 10:35:58 +0800

    Refactored DrawingViewer, tidying up.

--- src/viewer.py
+++ src/viewer.py
@@ -8,6 +8,12 @@ class DrawingViewer:
     def __init__(self, height: int, width: int):
         self._image = Image.new(mode="P", size=(width, height), color=Ink.WHITE.value)
 
+        self._palette = [
+            255, 255, 255,  # 0 = WHITE
+            0, 0, 0,  # 1 = BLACK
+            255, 255, 0  # 2 = YELLOW
+        ]
+
     def __enter__(self) -> ImageDraw:
         return ImageDraw.Draw(self._image)
 
@@ -17,19 +23,14 @@ class DrawingViewer:
         except RuntimeError:
             self._view_on_screen()
 
-    def _view_on_screen(self):
-        palette = [
-            255, 255, 255,  # 0 = WHITE
-            0, 0, 0,  # 1 = BLACK
-            255, 255, 0  # 2 = YELLOW
-        ]
-        self._image.putpalette(palette)
-        converted = self._image.convert(mode="RGB")
-        converted.show()
-
     def _view_on_inky(self):
         display = auto()
         display.set_border(display.WHITE)
         display.set_image(self._image)
         display.show()
+
+    def _view_on_screen(self):
+        self._image.putpalette(self._palette)
+        converted = self._image.convert(mode="RGB")
+        converted.show()
 
