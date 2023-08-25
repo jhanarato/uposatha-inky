0d0cdeeca1a021aee32eaedf3e2cab279b5a86b9
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-25 10:22:43 +0800

    Glyph metrics test use letter extending below baseline.

--- tests/test_glyph_metrics.py
+++ tests/test_glyph_metrics.py
@@ -6,19 +6,19 @@ from glyph_metrics import glyph_metrics
 
 @pytest.fixture
 def metrics():
-    return glyph_metrics(font_roboto.RobotoBold, "H")
+    return glyph_metrics(font_roboto.RobotoBold, "y")
 
 
 def test_glyph_width_in_units(metrics):
-    assert metrics.width.units() == 1186
+    assert metrics.width.units() == 1021
 
 
 def test_glyph_width_in_em(metrics):
-    assert metrics.width.to_em() == 0.5791015625
+    assert metrics.width.to_em() == 0.49853515625
 
 
 def test_glyph_width_in_points(metrics):
-    assert metrics.width.to_points(font_size=16) == 9.265625
+    assert metrics.width.to_points(font_size=16) == 7.9765625
 
 
 def test_width_is_xmin_xmax_difference(metrics):
