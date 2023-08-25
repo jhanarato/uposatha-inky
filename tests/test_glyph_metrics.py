16729b54b77310aa4f5e28637ce6c6abc79a7dca
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-25 10:37:09 +0800

    Added explicit pixels conversion to DesignUnits.

--- tests/test_glyph_metrics.py
+++ tests/test_glyph_metrics.py
@@ -22,6 +22,10 @@ def test_glyph_width_in_points(metrics):
     assert metrics.width.to_points(font_size=16) == 7.9765625
 
 
+def test_glyph_width_to_pixels(metrics):
+    assert metrics.width.to_pixels(font_points_per_em=16) == 8
+
+
 def test_width_is_xmin_xmax_difference(metrics):
     assert metrics.width == DesignUnits(1025 - 4, 2048)
 
