5e1eec9f846744a68008eff047dedebad805cb42
Ajahn Jhanarato <jhanarato@gmail.com>
2023-08-27 13:04:48 +0800

    Metrics fixtures clean up after themselves.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,20 @@
+import pytest
+
+from font_metrics import MetricsFromFile, MetricsPrecalculated
+from fonts import Font
 
 
+@pytest.fixture
+def metrics_reads_file():
+    current = Font.metrics
+    Font.metrics = MetricsFromFile()
+    yield
+    Font.metrics = current
+
+
+@pytest.fixture
+def metrics_precalculates():
+    current = Font.metrics
+    Font.metrics = MetricsPrecalculated()
+    yield
+    Font.metrics = current
