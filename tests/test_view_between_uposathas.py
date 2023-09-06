5337b2e9901a6bf7cd452948d1898c1f1850274e
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-06 10:38:38 +0800

    Tests use context fixture (#12)

--- tests/test_view_between_uposathas.py
+++ tests/test_view_between_uposathas.py
@@ -1,10 +1,9 @@
 from datetime import date
 
 import pytest
-from uposatha.elements import MoonPhase
 
 from components import Text, HorizontalLine
-from content import NextUposatha
+from content import get_context, next_uposatha_content
 from countdown import Appearance
 from countdown import Countdown
 from screen import WIDTH, HEIGHT
@@ -13,23 +12,19 @@ from views import BetweenUposathasView
 
 
 @pytest.fixture
-def content():
-    return NextUposatha(
-        today=date(2000, 1, 1),
-        falls_on=date(2000, 1, 2),
-        date="Date formatted", details="Details about uposatha",
-        moon_phase=MoonPhase.FULL,
-        fourteen_day=False
-    )
+def day_before_uposatha_context():
+    return get_context(date(2023, 9, 30))
 
 
-def test_should_show_between_view(content):
+def test_should_show_between_view(day_before_uposatha_context):
+    content = next_uposatha_content(day_before_uposatha_context.today)
     between_view = BetweenUposathasView(content)
     with DrawingViewer(width=WIDTH, height=HEIGHT, show=False) as draw:
         between_view.show(draw)
 
 
-def test_should_select_appearances(content):
+def test_should_select_appearances(day_before_uposatha_context):
+    content = next_uposatha_content(day_before_uposatha_context.today)
     between_view = BetweenUposathasView(content)
     appearances = between_view._appearances()
     assert appearances[1] == Appearance(
@@ -37,13 +32,15 @@ def test_should_select_appearances(content):
     )
 
 
-def test_should_generate_components(content):
+def test_should_generate_components(day_before_uposatha_context):
+    content = next_uposatha_content(day_before_uposatha_context.today)
     between_view = BetweenUposathasView(content)
     component_types = [type(component) for component in between_view._components()]
     assert component_types == [Text, HorizontalLine, Text, Countdown, Text]
 
 
-def test_should_layout_components(content):
+def test_should_layout_components(day_before_uposatha_context):
+    content = next_uposatha_content(day_before_uposatha_context.today)
     between_view = BetweenUposathasView(content)
     layout = between_view._layout()
     assert sum(1 for _ in layout.coordinates()) == len(between_view._components())
