8f1236c96c622c88c799dde4c41654010f46e33a
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-09 12:09:53 +0800

    update() leverages all my hard work.

--- src/update.py
+++ src/update.py
@@ -1,15 +1,13 @@
 from datetime import date
 
-import views
+import screen
 from content import get_context
+from viewer import DrawingViewer
+from views import select_view
 
 
 def update(today: date):
     context = get_context(today)
-
-    if context.holiday_today():
-        views.holiday(context)
-    elif context.uposatha_today():
-        views.uposatha(context)
-    else:
-        views.between_uposathas(context)
+    view = select_view(context)
+    with DrawingViewer(width=screen.WIDTH, height=screen.HEIGHT) as draw:
+        view.show(draw)
