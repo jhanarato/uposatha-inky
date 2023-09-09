cdad031f1422bb7271f504a88df73f7f570be7dc
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-09 12:12:00 +0800

    Moved update() to main script.

--- src/uposatha_inky.py
+++ src/uposatha_inky.py
@@ -1,7 +1,11 @@
 import argparse
 import datetime
+from datetime import date
 
-from update import update
+import screen
+from content import get_context
+from viewer import DrawingViewer
+from views import select_view
 
 
 def parse_args():
@@ -15,6 +19,13 @@ def parse_args():
                         help="Set today's date. Defaults to system date.")
     args = parser.parse_args()
     return args
+
+
+def update(today: date):
+    context = get_context(today)
+    view = select_view(context)
+    with DrawingViewer(width=screen.WIDTH, height=screen.HEIGHT) as draw:
+        view.show(draw)
 
 
 def main():
