bf5ff4f8edef4fa3fc43d73d626a42be9ec93002
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-09 11:59:50 +0800

    Merged uposatha_details() into BetweenUposathaContent.

--- src/views.py
+++ src/views.py
@@ -84,6 +84,13 @@ class BetweenUposathaContent:
     def fourteen_day(self) -> bool:
         return self._context.uposatha.days_since_previous == 14
 
+    def uposatha_details(self):
+        days_since_previous = self._context.uposatha.days_since_previous
+        uposatha_number = self._context.uposatha.number_in_season
+        number_of_uposathas = len(self._context.season.uposathas)
+        season_name = self._context.season.name.name.capitalize()
+        return f"{uposatha_number} of {number_of_uposathas} | {season_name} | {days_since_previous} Day"
+
 
 class BetweenUposathasView:
     GAP = 4
