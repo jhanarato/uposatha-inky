9d57bacda74edee55c94b34bb8bc2db8e41fa4d0
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-10 11:57:29 +0800

    Stubbed Pane class. (#30)

---
+++ src/panes.py
@@ -0,0 +1,8 @@
+from bbox import BBox
+from components import Drawable
+from layout import VerticalLayout
+
+
+class Pane:
+    def __init__(self, bbox: BBox, layout: VerticalLayout, components: list[Drawable]):
+        pass
---
+++ tests/test_panes.py
@@ -0,0 +1,13 @@
+from bbox import BBox
+from components import Text
+from fonts import Font
+from layout import VerticalLayout
+from panes import Pane
+from screen import Ink
+
+
+def test_create_pane():
+    bbox = BBox(0, 0, 100, 100)
+    layout = VerticalLayout(bbox.height, bbox.width)
+    components = [Text("Text to show", Font("roboto-bold", 30), Ink.BLACK)]
+    pane = Pane(bbox, layout, components)
