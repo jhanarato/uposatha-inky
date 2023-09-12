a86476a9dc8ec4da1c9c73a6953fad5cbb43968e
Ajahn Jhanarato <jhanarato@gmail.com>
2023-09-12 15:49:56 +0800

    Extracted bbox temp. (#30)

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -30,7 +30,8 @@ def test_should_draw_component_with_space_before():
 
 
 def test_three_components_spaced():
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_centred(Area(height=20, width=20))
     layout.add_space(50)
     layout.add_centred(Area(height=30, width=20))
@@ -41,27 +42,31 @@ def test_three_components_spaced():
 
 def test_should_centre_align_area():
     area = Area(height=10, width=20)
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_centred(area)
     assert list(layout.coordinates()) == [(90, 0)]
 
 
 def test_should_left_align_area():
     area = Area(height=10, width=20)
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_left(area)
     assert list(layout.coordinates()) == [(0, 0)]
 
 
 def test_should_right_align_area():
     area = Area(height=10, width=20)
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_right(area)
     assert list(layout.coordinates()) == [(180, 0)]
 
 
 def test_should_generate_coordinates():
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_centred(Area(height=20, width=20))
     layout.add_space(50)
     layout.add_centred(Area(height=30, width=20))
@@ -73,18 +78,21 @@ def test_should_generate_coordinates():
 
 
 def test_should_handle_no_components():
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     assert list(layout.coordinates()) == []
 
 
 def test_should_handle_space_only():
-    layout = VerticalLayout(BBox(top=0, left=0, bottom=100, right=200), 0)
+    bbox = BBox(top=0, left=0, bottom=100, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_space(50)
     assert list(layout.coordinates()) == []
 
 
 def test_should_layout_in_vertically_offset_bbox():
-    layout = VerticalLayout(BBox(top=100, left=0, bottom=200, right=200), 0)
+    bbox = BBox(top=100, left=0, bottom=200, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_centred(Area(height=20, width=100))
     layout.add_space(50)
     layout.add_centred(Area(height=30, width=100))
@@ -92,13 +100,15 @@ def test_should_layout_in_vertically_offset_bbox():
 
 
 def test_should_align_left_to_bbox():
-    layout = VerticalLayout(BBox(top=0, left=100, bottom=300, right=200), 0)
+    bbox = BBox(top=0, left=100, bottom=300, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_left(Area(height=50, width=70))
     assert list(layout.coordinates()) == [(100, 0)]
 
 
 def test_should_align_right_to_bbox():
-    layout = VerticalLayout(BBox(top=0, left=100, bottom=300, right=200), 0)
+    bbox = BBox(top=0, left=100, bottom=300, right=200)
+    layout = VerticalLayout(bbox, spacing=0)
     layout.add_right(Area(height=50, width=70))
     assert list(layout.coordinates()) == [(130, 0)]
 
